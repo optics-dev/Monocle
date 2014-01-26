743dc1bde31f67f0bac833e662fa06b4cf17167f
julien-truffaut <truffaut.julien@gmail.com>
2014-01-26 19:33:47 +0000

    Update syntax example to show result

--- examples/src/main/scala/lens/SyntaxExample.scala
+++ examples/src/main/scala/lens/SyntaxExample.scala
@@ -13,21 +13,19 @@ object SyntaxExample extends App {
   import lens.std.Map._
 
   val p = Person(23, "roger", Address("London", "EC1...", Location(2, 10)))
-  val newPerson = p >- age modify(_ + 2)
-  println(newPerson)
+  println(p >- age modify(_ + 2))
 
   val map = Map("Paris" -> Location(12, 34), "London" -> Location(10, 50))
 
-  println(map >- at("Paris") get)
-  println(map >- at("Pairs") set Some(Location(14, 18)))
-  println(map >- at("Pairs") set None)
-  println(map >- at("Roma") get)
+  println(map >- at("Paris") get)                        // Some(Location(12, 34))
+  println(map >- at("Roma") get)                         // None
+  println(map >- at("Pairs") set Some(Location(14, 18))) // Map("Paris" -> Location(14, 18), "London" ...)
+  println(map >- at("Pairs") set None)                   // Map("London" -> Location(10, 50))
+  println(map >- at("Paris") >- option[Location] >- latitude set 89) // Map("Paris" -> Location(89, 18), "London" ...)
 
-  println(map >- at("Paris") >- option[Location] >- latitude set 89)
-
-  println(List(1,2,3) ->- list[Int] set 3)          // ==> List(3,3,3)
-  println(List(1,2,3) ->- list[Int] modify (_ + 1)) // ==> List(2,3,4)
+  println(List(1,2,3) ->- list[Int] set 3)                    // ==> List(3,3,3)
+  println(List(1,2,3) ->- list[Int] modify (_ + 1))           // ==> List(2,3,4)
   println(List(1,2) ->- list[Int] lift (l => List(l-1, l+1))) // ==> List(List(0, 1), List(0, 3), List(2, 1), List(2, 3))
-  println(List(1,2) ->- list[Int] lift (l => Option(l))) // ==> Some(List(1,2))
+  println(List(1,2) ->- list[Int] lift (l => Option(l)))      // ==> Some(List(1,2))
 
 }
