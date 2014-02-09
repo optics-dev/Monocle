178858e966de0be8403c177668bd90221c9c4ab4
julien-truffaut <truffaut.julien@gmail.com>
2014-02-09 10:42:26 +0000

    Add Map example

---
+++ examples/src/main/scala/monocle/MapExample.scala
@@ -0,0 +1,18 @@
+package monocle
+
+import monocle.std.Map._
+
+
+object MapExample extends App {
+
+  val map = Map(1 -> "One", 2 -> "Two")
+
+  val atFirst = at[Int, String](1)
+
+  println(atFirst.get(map)) // => Some(One)
+
+  println(atFirst.set(map, None)) // delete 1
+
+  println(atFirst.set(map, Some("Un"))) // replace One by Un
+
+}
