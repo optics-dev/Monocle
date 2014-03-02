4a5f69e0d744457c71bbd894f058afadc6f5dfdf
Ross Huggett <rossh@magnetisegroup.com>
2014-03-02 13:21:37 +0000

    #12 - Finished off asInstanceOf removal

--- core/src/main/scala/monocle/bits/Bits.scala
+++ core/src/main/scala/monocle/bits/Bits.scala
@@ -53,23 +53,22 @@ object Bits {
     def negate(a: Int): Int = ~ a
   }
 
-  // todo: can we avoid down casting ?
   implicit val charInstance : Bits[Char] = new Bits[Char] {
 
     val bitSize: Int = 16
 
     def bitwiseOr(a1: Char, a2: Char): Char = (a1 | a2).toChar
-    def bitwiseAnd(a1: Char, a2: Char): Char = (a1 & a2).asInstanceOf[Char]
-    def bitwiseXor(a1: Char, a2: Char): Char = (a1 ^ a2).asInstanceOf[Char]
+    def bitwiseAnd(a1: Char, a2: Char): Char = (a1 & a2).toChar
+    def bitwiseXor(a1: Char, a2: Char): Char = (a1 ^ a2).toChar
 
-    def shiftL(a: Char, n: Int): Char = (a << n).asInstanceOf[Char]
-    def shiftR(a: Char, n: Int): Char = (a >> n).asInstanceOf[Char]
+    def shiftL(a: Char, n: Int): Char = (a << n).toChar
+    def shiftR(a: Char, n: Int): Char = (a >> n).toChar
 
-    def singleBit(n: Int): Char = (1 << n).asInstanceOf[Char]
+    def singleBit(n: Int): Char = (1 << n).toChar
 
     def testBit(a: Char, n: Int): Boolean = bitwiseAnd(a, singleBit(n)) != 0
 
-    def negate(a: Char): Char = (~ a).asInstanceOf[Char]
+    def negate(a: Char): Char = (~ a).toChar
     def signed(a: Char): Boolean = a.signum > 0
   }
 
