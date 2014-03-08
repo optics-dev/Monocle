ac2af1020365a11ea0cd7fbbf79ddfc6d77b4832
Ross Huggett <rossh@magnetisegroup.com>
2014-03-08 14:32:01 +0000

    #1 - Added lense implementation for tuples

---
+++ core/src/test/scala/monocle/TuplesSpec.scala
@@ -0,0 +1,12 @@
+package monocle
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ross
+ * Date: 08/03/2014
+ * Time: 11:46
+ * To change this template use File | Settings | File Templates.
+ */
+class TuplesSpec {
+
+}
