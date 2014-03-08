50039ebc4e79d1ad22ac8b138e8a27550c65b2b6
Ross Huggett <rossh@magnetisegroup.com>
2014-03-08 14:32:32 +0000

    #1 - Added lense implementation for tuples

--- core/src/main/scala/monocle/tuple/package.scala
+++ core/src/main/scala/monocle/tuple/package.scala
@@ -1,12 +1,12 @@
 package monocle
 
-/**
- * Created with IntelliJ IDEA.
- * User: ross
- * Date: 08/03/2014
- * Time: 14:28
- * To change this template use File | Settings | File Templates.
- */
 package object tuple {
+
+  def pairToSecondArg[S, T]: SimpleLens[(S, T), T] = {
+    def get(pair: (S, T)): T = pair._2
+    def set(pair: (S, T), newValue: T) : (S, T) = pair.copy(_2 = newValue)
+
+    SimpleLens(get, set)
+  }
 
 }
--- core/src/test/scala/monocle/TuplesSpec.scala
+++ core/src/test/scala/monocle/TuplesSpec.scala
@@ -1,12 +1,13 @@
 package monocle
 
-/**
- * Created with IntelliJ IDEA.
- * User: ross
- * Date: 08/03/2014
- * Time: 11:46
- * To change this template use File | Settings | File Templates.
- */
-class TuplesSpec {
+import org.specs2.scalaz.Spec
+import scalaz.Equal
 
+class TuplesSpec extends Spec {
+  import monocle.tuple._
+
+  implicit val intStringEqual = Equal.equalA[(Int, String)]
+  implicit val stringEqual = Equal.equalA[(String)]
+
+  checkAll(Lens.laws(pairToSecondArg[Int, String]))
 }
