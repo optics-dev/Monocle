f196cf0cf6731c97ce9e0f42a75fcf3dd88775cb
Ross Huggett <rossh@magnetisegroup.com>
2014-03-08 15:46:44 +0000

    #24 -

--- core/src/main/scala/monocle/tuple/package.scala
+++ core/src/main/scala/monocle/tuple/package.scala
@@ -2,6 +2,13 @@ package monocle
 
 package object tuple {
 
+  def pairToFirstArg[S, T]: SimpleLens[(S, T), S] = {
+    def get(pair: (S, T)): S = pair._1
+    def set(pair: (S, T), newValue: S) : (S, T) = pair.copy(_1 = newValue)
+
+    SimpleLens(get, set)
+  }
+
   def pairToSecondArg[S, T]: SimpleLens[(S, T), T] = {
     def get(pair: (S, T)): T = pair._2
     def set(pair: (S, T), newValue: T) : (S, T) = pair.copy(_2 = newValue)
