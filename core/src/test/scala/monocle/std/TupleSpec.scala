507507e94bf00f6ba4b0540acdbd08e5fe24d2a8
julien-truffaut <truffaut.julien@gmail.com>
2014-03-08 23:32:58 +0000

    #27 - Update TupleSpec tests following recent change from SimpleLens to Lens

--- core/src/test/scala/monocle/std/TupleSpec.scala
+++ core/src/test/scala/monocle/std/TupleSpec.scala
@@ -10,6 +10,6 @@ class TupleSpec extends Spec {
 
   implicit val intStringEqual = Equal.equalA[(Int, String)]
 
-  checkAll(Lens.laws(_1[Int, String]))
-  checkAll(Lens.laws(_2[Int, String]))
+  checkAll(Lens.laws(_1[Int, String, Int]))
+  checkAll(Lens.laws(_2[Int, String, String]))
 }
