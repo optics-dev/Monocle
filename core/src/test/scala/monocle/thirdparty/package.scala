fc84f8bcddea54574405fa46e692f204556ceefb
julien-truffaut <truffaut.julien@gmail.com>
2014-03-16 20:38:37 +0000

    Add tests for HList Iso and Generic lenses

---
+++ core/src/test/scala/monocle/thirdparty/package.scala
@@ -0,0 +1,28 @@
+package monocle
+
+import monocle.thirdparty.hlist._
+import shapeless.{Generic, HNil}
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
+import scalaz.Equal
+
+
+package object thirdparty {
+
+  case class Example(i: Int, s: String)
+  type IntStringHList = HL[Int, HL[String, HNil]]
+
+  implicit val gen = Generic.product[Example]
+
+  implicit val arbitraryExample = Arbitrary(for {
+    i <- arbitrary[Int]
+    s <- arbitrary[String]
+  } yield Example(i, s))
+
+  implicit val arbitraryHListIntString: Arbitrary[IntStringHList] =
+    Arbitrary(arbitrary[Example].map(toHListIso[Example, IntStringHList].get))
+
+  implicit val equalExample        = Equal.equalA[Example]
+  implicit val equalHListIntString = Equal.equalA[IntStringHList]
+
+}
