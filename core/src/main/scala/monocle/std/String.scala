fb2c73a57d3193dd97e86e1b9d57bf74b12f63b7
NightRa <ilan3580@gmail.com>
2014-03-27 18:21:21 +0200

    Abstracts constructing prisms for exception raising conversions.

--- core/src/main/scala/monocle/std/String.scala
+++ core/src/main/scala/monocle/std/String.scala
@@ -1,16 +1,17 @@
 package monocle.std
 
-import monocle.SimplePrism
-import scala.util.Try
+import monocle.util.TryPrism._
 
 object string extends StringInstances
 
 trait StringInstances {
-  def stringToBoolean: SimplePrism[String, Boolean] = SimplePrism(_.toString, s => Try(s.toBoolean).toOption)
-  def stringToByte: SimplePrism[String, Byte] = SimplePrism(_.toString, s => Try(s.toByte).toOption)
-  def stringToShort: SimplePrism[String, Short] = SimplePrism(_.toString, s => Try(s.toShort).toOption)
-  def stringToInt: SimplePrism[String, Int] = SimplePrism(_.toString, s => Try(s.toInt).toOption)
-  def stringToLong: SimplePrism[String, Long] = SimplePrism(_.toString, s => Try(s.toLong).toOption)
-  def stringToFloat: SimplePrism[String, Float] = SimplePrism(_.toString, s => Try(s.toFloat).toOption)
-  def stringToDouble: SimplePrism[String, Double] = SimplePrism(_.toString, s => Try(s.toDouble).toOption)
+  def stringCast[A](unsafe: String => A) = trySimplePrism[String, A](_.toString, unsafe)
+
+  def stringToBoolean = stringCast(_.toBoolean)
+  def stringToByte = stringCast(_.toByte)
+  def stringToShort = stringCast(_.toShort)
+  def stringToInt = stringCast(_.toInt)
+  def stringToLong = stringCast(_.toLong)
+  def stringToFloat = stringCast(_.toFloat)
+  def stringToDouble = stringCast(_.toDouble)
 }
