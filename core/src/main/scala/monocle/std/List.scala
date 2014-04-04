950475ad30234f36f8113b926e51174e8efd7b71
Ross Huggett <ross.huggett@gmail.com>
2014-04-04 22:26:15 +0100

    #30 - Added the implementation

--- core/src/main/scala/monocle/std/List.scala
+++ core/src/main/scala/monocle/std/List.scala
@@ -5,5 +5,12 @@ import monocle._
 object list extends ListInstances
 
 trait ListInstances {
-  def head[A] : SimpleLens[List[A], Option[A]] = ???
+  def head[A] : SimpleLens[List[A], Option[A]] = {
+    def _get(list: List[A]): Option[A] = list.headOption
+    def _set(list: List[A], option: Option[A]): List[A] =
+      if(list.isEmpty) option.toList else option.toList ::: list.tail
+    SimpleLens[List[A], Option[A]](_get, _set)
+  }
+
+
 }
--- examples/src/main/scala/monocle/ListExample.scala
+++ examples/src/main/scala/monocle/ListExample.scala
@@ -4,8 +4,8 @@ import monocle.std.list._
 
 object ListExample extends App {
 
-  head.get(List(1,2,3)) == Some(1)
-  head.set(List(1,2,3), Some(0)) == List(0,2,3)
-  head.set(List(1,2,3), None) == List(2,3) // delete
-  head.set(Nil, Some(2)) == Nil
+  println(head.get(List(1,2,3))) // Some(1)
+  println(head.set(List(1,2,3), Some(0))) // List(0,2,3)
+  println(head.set(List(1,2,3), None)) // List(2,3)
+  println(head.set(Nil, Some(2))) // List(2)
 }
