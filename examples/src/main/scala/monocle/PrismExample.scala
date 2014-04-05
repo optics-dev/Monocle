4c5d86f43b05061a94132607f08e039e7e31f25d
julien-truffaut <truffaut.julien@gmail.com>
2014-04-05 14:37:40 +0100

    Add option Example

--- examples/src/main/scala/monocle/PrismExample.scala
+++ examples/src/main/scala/monocle/PrismExample.scala
@@ -7,14 +7,6 @@ import scalaz.{ \/-, -\/ }
 
 object PrismExample extends App {
 
-  println( some.getOption(Some(1)) ) // Some(1)
-  println( some.set(None, 2) )       // None
-  println( some.set(Some(1), 'a') )  // Some('a')
-  println( some.modify(Some(1), { n: Int => n + 2.0 }) ) // Some(3.0)
-
-  println( none.getOption(Some(1)) ) // None
-  println( none.getOption(None) )    // Some(())
-
   println( left.getOption(-\/(1)) )   // Some(1)
   println( left.getOption(\/-(1)) )   // None
 
---
+++ examples/src/test/scala/monocle/OptionExample.scala
@@ -0,0 +1,35 @@
+package monocle
+
+import org.specs2.scalaz.Spec
+import monocle.std.option._
+
+
+class OptionExample extends Spec {
+
+  "some creates a Prism from an Option to its element" in {
+
+    some.getOption(Some(1)) shouldEqual Some(1)
+    some.reverseGet(1)      shouldEqual Some(1)
+
+    // type can be changed with set and modify
+    some.set(Some(1), 'a')  shouldEqual Some('a')
+    some.set(None, 2)       shouldEqual None
+
+    some.modify(Some(1), { (_: Int) + 2.0 }) shouldEqual Some(3.0)
+
+  }
+
+  "none creates a Prism from an Option to Unit" in {
+
+    none.getOption(None)    shouldEqual Some(())
+    none.getOption(Some(2)) shouldEqual None
+
+    none.reverseGet(()) shouldEqual None
+
+    // none setter doesn't do anything
+    none.set(Some(1), ()) shouldEqual Some(1)
+    none.set(None   , ()) shouldEqual None
+
+  }
+
+}
