a9464c75cd522212403c102ccbcbf5177cad4b05
julien-truffaut <truffaut.julien@gmail.com>
2014-04-07 22:44:52 +0100

    Add filterIndexes

--- core/src/main/scala/monocle/util/Index.scala
+++ core/src/main/scala/monocle/util/Index.scala
@@ -8,18 +8,29 @@ trait Index[S, I, A] {
   /** Creates a Traversal from S to 0 or 1 A */
   def index(i: I): SimpleTraversal[S, A]
 
+  def filterIndexes(predicate: I => Boolean): SimpleTraversal[S, A]
+
 }
 
 object Index {
 
   def index[S, I, A](i: I)(implicit ev: Index[S, I, A]): SimpleTraversal[S, A] = ev.index(i)
 
+  def filterIndexes[S, I, A](predicate: I => Boolean)
+                            (implicit ev: Index[S, I, A]): SimpleTraversal[S, A] = ev.filterIndexes(predicate)
+
   implicit def indexedMap[K, V] = new Index[Map[K, V], K, V] {
     def index(i: K): SimpleTraversal[Map[K, V], V] = {
       import monocle.syntax.traversal._
       monocle.std.map.at[K, V](i) |->> monocle.std.option.some
     }
+
+    def filterIndexes(predicate: K => Boolean) = new Traversal[Map[K, V], Map[K, V], V, V]{
+      def multiLift[F[_] : Applicative](from: Map[K, V], f: V => F[V]): F[Map[K, V]] =
+        Applicative[F].map(filterLiftList(from.toList, f)(predicate))(_.toMap)
+    }
   }
+
 
   implicit def indexWithSplit[S, I, A](implicit ev: Split[S, I, A]): Index[S, I, A] = new Index[S, I, A]{
     def index(i: I): SimpleTraversal[S, A] = new Traversal[S, S, A, A] {
@@ -29,6 +40,10 @@ object Index {
         }.getOrElse(Applicative[F].point(from))
     }
 
+    def filterIndexes(predicate: I => Boolean): SimpleTraversal[S, A] = new Traversal[S, S, A, A] {
+      def multiLift[F[_] : Applicative](from: S, f: A => F[A]): F[S] =
+        Applicative[F].map(filterLiftList(ev.toIndexedList(from), f)(predicate))(ev.fromIndexedList)
+    }
   }
 
   /** Split is type class to facilitate creation of Index*/
@@ -42,6 +57,10 @@ object Index {
 
     def merge(before: S, at: A, after: S): S
 
+    def toIndexedList(from: S): List[(I, A)]
+
+    def fromIndexedList(list: List[(I, A)]): S
+
   }
 
   implicit def listSplit[A] = new Split[List[A], Int, A] {
@@ -52,16 +71,30 @@ object Index {
         case (before, at :: after) => Some(before, at, after)
         case _                     => None
       }
+
+    def fromIndexedList(list: List[(Int, A)]): List[A] = list.map(_._2)
+
+    def toIndexedList(from: List[A]): List[(Int, A)] = from.zipWithIndex.map(_.swap)
   }
 
   implicit val stringSplit = new Split[String, Int, Char] {
     def merge(before: String, at: Char, after: String): String = before + at + after
+
     def split(from: String, index: Int): Option[(String, Char, String)] = {
       val (before, atAndAfter) = from.splitAt(index)
       val (at, after)          = atAndAfter.splitAt(1)
       at.headOption.map((before, _, after))
     }
+
+    def fromIndexedList(list: List[(Int, Char)]): String = list.map(_._2).mkString("")
+
+    def toIndexedList(from: String): List[(Int, Char)] = from.toList.zipWithIndex.map(_.swap)
   }
+
+  private def filterLiftList[I, A, F[_]](list: List[(I, A)], f: A => F[A])(predicate: I => Boolean)
+                                        (implicit ev: Applicative[F]): F[List[(I, A)]] =
+    list.map{ case (i, a) => ev.map(if(predicate(i)) f(a) else ev.point(a))(i -> _) }
+      .foldRight(ev.point(List.empty[(I, A)]))( ev.apply2(_, _)(_ :: _) )
 
 
 }
--- examples/src/test/scala/monocle/IndexExample.scala
+++ examples/src/test/scala/monocle/IndexExample.scala
@@ -15,6 +15,14 @@ class IndexExample extends Spec {
 
   }
 
+  "filterIndexes creates Traversal from a Map to all values where the index matches the predicate" in {
+
+    (Map("One" -> 1, "Two" -> 2) |->> filterIndexes{k: String => k.toLowerCase.contains("o")} getAll) shouldEqual List(1, 2)
+
+    (Map("One" -> 1, "Two" -> 2) |->> filterIndexes{k: String => k.startsWith("T")} set 3) shouldEqual Map("One" -> 1, "Two" -> 3)
+
+  }
+
   "index creates 0 or 1 Traversal from a List to a value" in {
 
     (List(0,1,2,3) |->> index(1) headOption) shouldEqual Some(1)
@@ -23,6 +31,14 @@ class IndexExample extends Spec {
     (List(0,1,2,3) |->> index(1) modify(_ + 1)) shouldEqual List(0,2,2,3)
     // setting or modifying a value at an index without value is a no op
     (List(0,1,2,3) |->> index(64) set 10)       shouldEqual List(0,1,2,3)
+
+  }
+
+  "filterIndexes creates Traversal from a List to all values where the index matches the predicate" in {
+
+    (List(1,3,5,7) |->> filterIndexes{ i: Int => i%2 == 0 } getAll) shouldEqual List(1,5)
+
+    (List(1,3,5,7) |->> filterIndexes{ i: Int => i >= 2 } modify(_ + 2)) shouldEqual List(1,3,7,9)
 
   }
 
