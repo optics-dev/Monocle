300a509d5d20c2dae714e0603e4124494b8d90b5
Ross Huggett <ross.huggett@gmail.com>
2014-04-12 12:11:57 +0100

    Added lens for the last elements of the list

--- core/src/main/scala/monocle/std/List.scala
+++ core/src/main/scala/monocle/std/List.scala
@@ -16,5 +16,17 @@ trait ListInstances {
     SimpleLens[List[A], Option[A]](_get, _set)
   }
 
+  def last[A] : SimpleLens[List[A], Option[A]] = {
+    def _get(list: List[A]): Option[A] = list.lastOption
+    def _set(list: List[A], option: Option[A]): List[A] = (list, option) match {
+      case(Nil,   None  ) => Nil
+      case(Nil,   Some(a)) => a :: Nil
+      case(xs,    None  ) => xs.dropRight(1)
+      case(xs,    Some(a)) => xs.dropRight(1) ::: List(a)
+    }
+
+    SimpleLens[List[A], Option[A]](_get, _set)
+  }
+
 
 }
