ebfc5de33e82041e1d664a216dfdb2da4d824a3e
NightRa <ilan3580@gmail.com>
2014-04-15 23:59:50 +0300

    Fix ambiguous implicits for curry.

--- core/src/main/scala/monocle/function/Curry.scala
+++ core/src/main/scala/monocle/function/Curry.scala
@@ -6,22 +6,32 @@ trait Curry[F, G] {
   def curry: SimpleIso[F, G]
 }
 
-object Curry extends CurryInstances
+object Curry extends CurryInstances0
 
-trait CurryInstances {
-  implicit def curry2[A,B,C] = new Curry[(A,B) => C,A => B => C] {
+/**
+ * We do a trait inheritance hierarchy in order to solve ambiguous implicit resolution.
+ * The traits higher up (CurryInstances0) will get higher priority in implicit resolution.
+ **/
+trait CurryInstances0 extends CurryInstances1 {
+  implicit def curry5[A, B, C, D, E, F] = new Curry[(A, B, C, D, E) => F, A => B => C => D => E => F] {
     def curry = SimpleIso(_.curried, f => Function.uncurried(f))
   }
+}
 
-  implicit def curry3[A,B,C,D] = new Curry[(A,B,C) => D, A => B => C => D] {
+trait CurryInstances1 extends CurryInstances2 {
+  implicit def curry4[A, B, C, D, E] = new Curry[(A, B, C, D) => E, A => B => C => D => E] {
     def curry = SimpleIso(_.curried, f => Function.uncurried(f))
   }
+}
 
-  implicit def curry4[A,B,C,D,E] = new Curry[(A,B,C,D) => E, A => B => C => D => E] {
+trait CurryInstances2 extends CurryInstances3 {
+  implicit def curry3[A, B, C, D] = new Curry[(A, B, C) => D, A => B => C => D] {
     def curry = SimpleIso(_.curried, f => Function.uncurried(f))
   }
+}
 
-  implicit def curry5[A,B,C,D,E,F] = new Curry[(A,B,C,D,E) => F, A => B => C => D => E => F] {
+trait CurryInstances3 {
+  implicit def curry2[A, B, C] = new Curry[(A, B) => C, A => B => C] {
     def curry = SimpleIso(_.curried, f => Function.uncurried(f))
   }
 }
