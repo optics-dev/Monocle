520b77b3113dd824914df736d85dd3b104fa5b27
julien-truffaut <truffaut.julien@gmail.com>
2014-05-16 22:23:33 +0100

    Add reverse instance for HList

--- example/src/test/scala/monocle/HListExample.scala
+++ example/src/test/scala/monocle/HListExample.scala
@@ -6,6 +6,7 @@ import org.specs2.scalaz.Spec
 import shapeless.{HNil, ::}
 import monocle.syntax.lens._
 import monocle.function.Fields._
+import monocle.function.Reverse._
 
 
 class HListExample extends Spec {
@@ -24,6 +25,10 @@ class HListExample extends Spec {
     (Example(1, "bla", true) <-> toHList get) shouldEqual (1 :: "bla" :: true :: HNil)
 
     (Example(1, "bla", true) <-> toHList |-> _1 set 5) shouldEqual Example(5, "bla", true)
+  }
+
+  "reverse creates an Iso between an HList and its reverse version" in {
+    (1 :: "bla" :: true :: HNil <-> reverse get) shouldEqual (true :: "bla" :: 1 :: HNil)
   }
 
 }
