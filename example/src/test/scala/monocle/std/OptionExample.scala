fbe82d7a5a5402d6d8c9ca5bc8b93d454c9b02f3
Julien Truffaut <truffaut.julien@gmail.com>
2014-08-28 17:46:46 +0100

    Add missing type safe equality

--- example/src/test/scala/monocle/std/OptionExample.scala
+++ example/src/test/scala/monocle/std/OptionExample.scala
@@ -2,32 +2,34 @@ package monocle.std
 
 import org.specs2.scalaz.Spec
 
+import scalaz.Maybe
+
 
 class OptionExample extends Spec {
 
   "some creates a Prism from an Option to its element" in {
 
-    some.getMaybe(Some(1)) shouldEqual Some(1)
-    some.reverseGet(1)      shouldEqual Some(1)
+    some.getMaybe(Some(1)) ==== Maybe.just(1)
+    some.reverseGet(1)     ==== Some(1)
 
     // type can be changed with set and modify
-    some.set(Some(1), 'a')  shouldEqual Some('a')
-    some.set(None, 2)       shouldEqual None
+    some.set(Some(1), 'a')  ==== Some('a')
+    some.set(None, 2)       ==== None
 
-    some.modify(Some(1), { (_: Int) + 2.0 }) shouldEqual Some(3.0)
+    some.modify(Some(1), { (_: Int) + 2.0 }) ==== Some(3.0)
 
   }
 
   "none creates a Prism from an Option to Unit" in {
 
-    none.getMaybe(None)    shouldEqual Some(())
-    none.getMaybe(Some(2)) shouldEqual None
+    none.getMaybe(None)    ==== Maybe.just(())
+    none.getMaybe(Some(2)) ==== Maybe.empty
 
-    none.reverseGet(()) shouldEqual None
+    none.reverseGet(()) ==== None
 
     // none setter doesn't do anything
-    none.set(Some(1), ()) shouldEqual Some(1)
-    none.set(None   , ()) shouldEqual None
+    none.set(Some(1), ()) ==== Some(1)
+    none.set(None   , ()) ==== None
 
   }
 
--- example/src/test/scala/monocle/std/TreeExample.scala
+++ example/src/test/scala/monocle/std/TreeExample.scala
@@ -14,33 +14,30 @@ class TreeExample extends Spec with ScalazMatchers {
   val tree = node(1, Stream(leaf(2), leaf(3)))
 
   "label creates a Lens from a Tree to its root label" in {
-    (tree applyLens rootLabel get) shouldEqual 1
-
-    (tree applyLens rootLabel modify (_ - 1)) must equal (node(0, Stream(leaf(2), leaf(3))))
+    (tree applyLens rootLabel get) ==== 1
+    
+    (tree applyLens rootLabel modify (_ - 1)) ==== node(0, Stream(leaf(2), leaf(3)))
   }
 
   "subForest creates a Lens from a Tree to its children" in {
-    (leaf(1) applyLens subForest get) shouldEqual Stream.Empty
+    (leaf(1) applyLens subForest get) ==== Stream.Empty
+    (tree    applyLens subForest get) ==== Stream(leaf(2), leaf(3))
 
-    (tree applyLens subForest get) must equal (Stream(leaf(2), leaf(3)))
-
-    (tree applyLens rootLabel modify (_ - 1)) must equal (node(0, Stream(leaf(2), leaf(3))))
+    (tree applyLens rootLabel modify (_ - 1)) ==== node(0, Stream(leaf(2), leaf(3)))
   }
 
   "leftMostLeaf creates a Lens from a Tree to its left most leaf" in {
-    (leaf(1) applyLens leftMostLabel get) shouldEqual 1
+    (leaf(1) applyLens leftMostLabel get) ==== 1
+    (tree    applyLens leftMostLabel get) ==== 2
 
-    (tree    applyLens leftMostLabel get) shouldEqual 2
-
-    (tree    applyLens leftMostLabel set 0) must equal (node(1, Stream(leaf(0), leaf(3))))
+    (tree    applyLens leftMostLabel set 0) ==== node(1, Stream(leaf(0), leaf(3)))
   }
 
   "leftMostLeaf creates a Lens from a Tree to its right most leaf" in {
-    (leaf(1) applyLens rightMostLabel get) shouldEqual 1
+    (leaf(1) applyLens rightMostLabel get) ==== 1
+    (tree    applyLens rightMostLabel get) ==== 3
 
-    (tree    applyLens rightMostLabel get) shouldEqual 3
-
-    (tree    applyLens rightMostLabel set 0) must equal (node(1, Stream(leaf(2), leaf(0))))
+    (tree    applyLens rightMostLabel set 0) ==== node(1, Stream(leaf(2), leaf(0)))
   }
 
 
