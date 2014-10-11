42c4d22a8699b606ca1280260376354cd36685e2
Julien Truffaut <truffaut.julien@gmail.com>
2014-10-11 13:16:04 +0100

    Resolve merge conflict

--- core/src/main/scala/monocle/std/These.scala
+++ core/src/main/scala/monocle/std/These.scala
@@ -10,12 +10,12 @@ import scalaz.syntax.either._
 object these extends TheseFunctions
 
 trait TheseFunctions {
-  def theseDisjunction[A, B]: SimplePrism[A \&/ B, A \/ B] = SimplePrism({
+  def theseDisjunction[A, B]: SimplePrism[A \&/ B, A \/ B] = SimplePrism[A \&/ B, A \/ B]{
     case This(a) => a.left[B].just
     case That(b) => b.right[A].just
     case Both(_, _) => Maybe.empty[A \/ B]
-  }, {
+  }{
     case -\/(a) => This(a)
     case \/-(b) => That(b)
-  })
+  }
 }
