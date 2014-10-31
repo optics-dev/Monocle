3efb3ba67e2c0b66dadc120f9e4afd9d61ac5768
Julien Truffaut <truffaut.julien@gmail.com>
2014-10-31 23:05:13 +0000

    Update Lens comments

--- core/src/main/scala/monocle/Lens.scala
+++ core/src/main/scala/monocle/Lens.scala
@@ -9,28 +9,27 @@ import scalaz.{ Applicative, Functor, Monoid, Profunctor, Tag }
  * A [[PLens]] defines a purely functional relation between a type S and an smaller type A.
  * A [[PLens]] can be seen as a pair of function get and set defined as follow:
  *  get: S      => A  i.e. from an S, we can extract an A
- *  set: (B, S) => T  i.e. if we set a B in a S, we obtain a T
+ *  set: (B, S) => T  i.e. if we replace an A by a B in a S, we obtain a T
  *
- * [[PLens]] stands for polymorphic [[Lens]] as it set and modify methods change a type A to B.
- * [[Lens]] is a type alias for [[PLens]] that only permits monomoprhic updates:
+ * [[PLens]] stands for Polymorphic Lens as it set and modify methods change a type A to B and S to T.
+ * [[Lens]] is a type alias for [[PLens]] restricted to monomoprhic updates:
  *
  * type Lens[S, A] = PLens[S, S, A, A]
  *
- * A [[PLens]] is typically between a [[Product]] type and one of it is component, e.g. case class, tuple, HList
- *
  * A [[PLens]] is also a valid [[Getter]], [[Fold]], [[Optional]], [[Traversal]] and [[Setter]]
+ *
+ * Typically a [[PLens]] or [[Lens]] can be defined between a [[Product]] (e.g. case class, tuple, HList)
+ * and one of it is component.
  */
 abstract class PLens[S, T, A, B] { self =>
 
-  /** underlying representation of a [[PLens]], all [[PLens]] methods are defined in terms of _lens */
+  /** underlying representation of [[PLens]], all [[PLens]] methods are defined in terms of _lens */
   def _lens[P[_, _]: Strong]: Optic[P, S, T, A, B]
 
   /** get the target of [[PLens]] */
   @inline final def get(s: S): A = _lens[Forget[A, ?, ?]].apply(Forget(identity)).runForget(s)
 
-  /**
-   * modify polymorphically the target of a [[PLens]] using [[Functor]] function
-   */
+  /** modify polymorphically the target of a [[PLens]] using [[Functor]] function */
   @inline final def modifyF[F[_]: Functor](f: A => F[B])(s: S): F[T] =
     Tag.unwrap(_lens[UpStar[F, ?, ?]](Strong.upStarStrong[F])(UpStar[F, A, B](f))).apply(s)
 
@@ -72,9 +71,9 @@ abstract class PLens[S, T, A, B] { self =>
     @inline def foldMap[M: Monoid](f: A => M)(s: S): M = f(get(s))
   }
   /** view a [[PLens]] as a [[Getter]] */
-  @inline final def asGetter: Getter[S, A] = Getter[S, A](get)
+  final def asGetter: Getter[S, A] = Getter[S, A](get)
   /** view a [[PLens]] as a [[Setter]] */
-  @inline final def asSetter: Setter[S, T, A, B] = Setter[S, T, A, B](modify)
+  final def asSetter: Setter[S, T, A, B] = Setter[S, T, A, B](modify)
   /** view a [[PLens]] as a [[Traversal]] */
   final def asTraversal: Traversal[S, T, A, B] = new Traversal[S, T, A, B] {
     @inline def _traversal[F[_]: Applicative](f: A => F[B])(s: S): F[T] = self.modifyF(f)(s)
