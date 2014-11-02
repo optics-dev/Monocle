933754a077589348dc88b8a6b12b8a1091ac4d45
Max Worgan <max@tactix4.com>
2014-11-02 17:15:52 +0000

    index refactored to use Head and Tail

--- core/src/main/scala/monocle/std/NonEmptyList.scala
+++ core/src/main/scala/monocle/std/NonEmptyList.scala
@@ -10,35 +10,39 @@ import scalaz.syntax.std.option._
 
 object nonemptylist extends NonEmptyListInstances
 
-trait NonEmptyListInstances {
-
-  def zipWithIndexNel[A](n: NonEmptyList[A]): NonEmptyList[(A, Int)] = {
-    n.zip(NonEmptyList(0, Stream.from(1): _*))
-  }
+trait NonEmptyListInstances{
 
   implicit def nelEach[A]: Each[NonEmptyList[A], A] = Each.traverseEach[NonEmptyList, A]
 
   implicit def nelIndex[A]: Index[NonEmptyList[A], Int, A] = new Index[NonEmptyList[A], Int, A] {
 
-    def index(i: Int) = SimpleOptional[NonEmptyList[A], A](
-      il => if (i < 0) Maybe.empty else il.list.drop(i).headOption.toMaybe)(
-        (a, il) => zipWithIndexNel(il).traverse[Id, A] {
-          case (_, index) if index == i => a
-          case (value, index)           => value
-        }
-      )
+    def index(i: Int): SimpleOptional[NonEmptyList[A], A] = i match {
+      case 0 => nelHead.head.asOptional
+      case _ => nelTail.tail composeOptional list.listIndex.index(i-1)
+    }
   }
 
   implicit def nelFilterIndex[A]: FilterIndex[NonEmptyList[A], Int, A] =
-    FilterIndex.traverseFilterIndex[NonEmptyList, A](n => zipWithIndexNel(n))
+    FilterIndex.traverseFilterIndex[NonEmptyList, A](n => n.zip(NonEmptyList(0, Stream.from(1):_*)))
 
   implicit def nelReverse[A]: Reverse[NonEmptyList[A], NonEmptyList[A]] =
     reverseFromReverseFunction[NonEmptyList[A]](_.reverse)
 
-}
+  implicit def nelFieldOne[A]: Field1[NonEmptyList[A], A] = new Field1[NonEmptyList[A],A] {
+    override def first: SimpleLens[NonEmptyList[A], A] =
+      SimpleLens[NonEmptyList[A], A](_.head)((h: A, list: NonEmptyList[A]) => list.<::(h))
+  }
+
+  implicit def nelHead[A]: Head[NonEmptyList[A], A] =  Head.field1Head[NonEmptyList[A],A]
+
+  implicit def nelTail[A] : Tail[NonEmptyList[A], List[A]] = new Tail[NonEmptyList[A], List[A]] {
+    override def tail = SimpleLens[NonEmptyList[A], List[A]](_.tail)(
+      (tail: List[A], nel: NonEmptyList[A]) => NonEmptyList(nel.head,tail:_*))
+  }
 
   implicit def nelAndOneIso[A] : SimpleIso[NonEmptyList[A], OneAnd[List,A]] =
     SimpleIso((nel: NonEmptyList[A]) => OneAnd[List,A](nel.head,nel.tail))(
               (and: OneAnd[List, A]) => NonEmptyList(and.head,and.tail:_*))
 
 
+}
