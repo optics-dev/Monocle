2d3e217e98f257e1a22009a63b539881c96ac472
Max Worgan <max@tactix4.com>
2014-11-04 11:58:51 +0000

    removed snoc and cons instances

--- core/src/main/scala/monocle/std/NonEmptyList.scala
+++ core/src/main/scala/monocle/std/NonEmptyList.scala
@@ -1,12 +1,10 @@
 package monocle.std
 
 import monocle.function._
-import monocle.{SimpleIso, SimpleLens, SimpleOptional, SimplePrism}
+import monocle.{SimpleIso, SimpleOptional}
 
 import scalaz.{OneAnd, NonEmptyList}
 import scalaz.NonEmptyList._
-import scalaz.syntax.std.list._
-import scalaz.syntax.std.option._
 
 object nonemptylist extends NonEmptyListInstances
 
@@ -28,20 +26,6 @@ trait NonEmptyListInstances{
   implicit def nelReverse[A]: Reverse[NonEmptyList[A], NonEmptyList[A]] =
     reverseFromReverseFunction[NonEmptyList[A]](_.reverse)
 
-  implicit def nelCons[A]: Cons[NonEmptyList[A], A] = new Cons[NonEmptyList[A], A]{
-    def cons = SimplePrism[NonEmptyList[A], (A, NonEmptyList[A])]{ l =>
-      l.tail.toNel.toMaybe.map(t => (l.head,t))
-    }{ case (a, s) => s.<::(a) }
-  }
-
-  implicit def nelSnoc[A]: Snoc[NonEmptyList[A], A] = new Snoc[NonEmptyList[A], A]{
-
-    override def snoc: SimplePrism[NonEmptyList[A], (NonEmptyList[A], A)] =
-      SimplePrism(
-        (nel: NonEmptyList[A]) =>nel.init.toNel.toMaybe.map(i => (i, nel.last))){
-        case (init, last) => init :::> List(last)}
-
-  }
 
   implicit def nelCons1[A]: Cons1[NonEmptyList[A], A, List[A]] =  new Cons1[NonEmptyList[A],A,List[A]]{
     def cons1 = SimpleIso((nel: NonEmptyList[A]) => (nel.head,nel.tail)){case (h,t) => NonEmptyList(h,t:_*)}
