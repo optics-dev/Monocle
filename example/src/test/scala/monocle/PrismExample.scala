a5a291056cbc8e200ef6da9e43badfd6fccebbde
Julien Truffaut <truffaut.julien@gmail.com>
2015-01-31 19:23:55 +0100

    Add GenPrism macro

--- example/src/test/scala/monocle/PrismExample.scala
+++ example/src/test/scala/monocle/PrismExample.scala
@@ -1,39 +1,44 @@
 package monocle
 
+import monocle.macros.{GenIso, GenPrism}
 import monocle.std._
 import org.specs2.scalaz.Spec
 
-import scalaz.Tags.Multiplication
 import scalaz._
-import scalaz.std.anyVal._
 
 
 class PrismExample extends Spec {
 
-  sealed trait Expression[T]
-  case class Atom[T](value: T) extends Expression[T]
-  case class Plus[T](left: Expression[T], right: Expression[T]) extends Expression[T]
+  sealed trait IntOrString
+  case class I(i: Int) extends IntOrString
+  case class S(s: String) extends IntOrString
 
-  def atom[A, B: Monoid] = PPrism[Expression[A], Expression[B], A, B]{
-    case Atom(v)    => \/-(v)
-    case Plus(_, _) => -\/(Atom(Monoid[B].zero))
-  }(Atom.apply[B])
+  val _i = GenPrism[IntOrString, I] composeIso GenIso[I, Int]
+  val _s = Prism[IntOrString, String]{case S(s) => Maybe.just(s); case _ => Maybe.empty}(S.apply)
 
-  val mAtom = atom[Int, Int @@ Multiplication]
+  "getMaybe return the target of a Prism, as it name implies, it can fail" in {
+    _i.getMaybe(I(1))  ==== Maybe.just(1)
+    _i.getMaybe(S("")) ==== Maybe.empty
 
-  "Prism getMaybe extracts the target (A) of a Prism if it exists" in {
-    mAtom.getMaybe(Atom(3))                ==== Maybe.just(3)
-    mAtom.getMaybe(Plus(Atom(1), Atom(2))) ==== Maybe.empty
+    _s.getMaybe(S("hello")) ==== Maybe.just("hello")
+    _s.getMaybe(I(10))      ==== Maybe.empty
   }
 
-  "Prism getOrModify extracts the target (A) of a Prism if it exists or it modifies the source (T)" in {
-    mAtom.getOrModify(Atom(3))                ==== \/-(3)
-    mAtom.getOrModify(Plus(Atom(1), Atom(2))) ==== -\/(Atom(Multiplication(1)))
+  "reverseGet return the source of Prism, in this case the constructor of IntOrString" in {
+    _i.reverseGet(3)     ==== I(3)
+    _s.reverseGet("Yop") ==== S("Yop")
   }
 
-  "Prism reverseGet transforms the modified target (B) in a modified source (T)" in {
-    mAtom.reverseGet(Multiplication(1)) ==== Atom(Multiplication(1))
+  "modify can alter the target of Prism, if there is no target modify has no effect" in {
+    _i.modify(_ + 1)(I(3))   ==== I(4)
+    _i.modify(_ + 1)(S(""))  ==== S("")
   }
+
+  "modify returns the original object with its target modified or nothing if there is no target" in {
+    _s.modifyMaybe(_.reverse)(S("hello")) ==== Maybe.just(S("olleh"))
+    _s.modifyMaybe(_.reverse)(I(3))       ==== Maybe.empty
+  }
+
 
   "intToChar is a Prism from Int to Char" in {
     intToChar.getMaybe(65)    ==== Maybe.just('A')
---
+++ macro/src/main/scala/monocle/macros/GenPrism.scala
@@ -0,0 +1,38 @@
+package monocle.macros
+
+import monocle.Prism
+import monocle.macros.internal.MacrosCompatibility
+
+
+
+
+object GenPrism {
+  def apply[S, A <: S]: Prism[S, A] = macro GenPrismImpl.genPrism_impl[S, A]
+}
+
+private object GenPrismImpl extends MacrosCompatibility {
+  def genPrism_impl[S: c.WeakTypeTag, A: c.WeakTypeTag](c: Context): c.Expr[Prism[S, A]] = {
+    import c.universe._
+
+    val (sTpe, aTpe) = (weakTypeOf[S], weakTypeOf[A])
+
+    val sTpeSym = companionTpe(c)(sTpe)
+    c.Expr[Prism[S, A]](q"""
+      import monocle.Iso
+      import scalaz.{\/, \/-, -\/, Maybe}
+
+      new Prism[$sTpe, $aTpe]{
+        def getOrModify(s: $sTpe): $sTpe \/ $aTpe =
+          if(s.isInstanceOf[$aTpe]) \/-(s.asInstanceOf[$aTpe])
+          else -\/(s)
+
+        def reverseGet(a: $aTpe): $sTpe =
+          a.asInstanceOf[$sTpe]
+
+        def getMaybe(s: $sTpe): Maybe[$aTpe] =
+          if(s.isInstanceOf[$aTpe]) Maybe.just(s.asInstanceOf[$aTpe])
+          else Maybe.empty
+      }
+    """)
+  }
+}
