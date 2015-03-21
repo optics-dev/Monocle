f26a0397d2ca6e21a79f750d1573428c2a547a8e
Julien Truffaut <truffaut.julien@gmail.com>
2015-03-21 17:37:10 +0100

    merge all apply syntax in a single file

--- core/src/main/scala/monocle/syntax/Syntaxes.scala
+++ core/src/main/scala/monocle/syntax/Syntaxes.scala
@@ -1,11 +1,4 @@
 package monocle.syntax
 
 trait Syntaxes
-  extends LensSyntax
-  with TraversalSyntax
-  with IsoSyntax
-  with PrismSyntax
-  with OptionalSyntax
-  with GetterSyntax
-  with FoldSyntax
-  with SetterSyntax
+  extends ApplySyntax
