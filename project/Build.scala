4458e0223ea1abe41e4abd38525c9ebb43b3814c
Julien Truffaut <truffaut.julien@gmail.com>
2015-06-14 14:24:32 +0200

    move sonatype.sbt into Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -157,6 +157,10 @@ object MonocleBuild extends Build {
       commitNextVersion,
       pushChanges
     ),
+    credentials ++= (for {
+      username <- Option(System.getenv().get("SONATYPE_USERNAME"))
+      password <- Option(System.getenv().get("SONATYPE_PASSWORD"))
+    } yield Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", username, password)).toSeq,
     pomExtra := {
       <url>https://github.com/julien-truffaut/Monocle</url>
         <licenses>
--- sonatype.sbt
+++
@@ -1,4 +0,0 @@
-credentials ++= (for {
-  username <- Option(System.getenv().get("SONATYPE_USERNAME"))
-  password <- Option(System.getenv().get("SONATYPE_PASSWORD"))
-} yield Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", username, password)).toSeq
