0558b78c7abf4b39662e0e97fc99c93381207f75
Julien Truffaut <truffaut.julien@gmail.com>
2015-07-04 20:52:24 +0200

    #181 add monomorphic aliases for validations

--- core/src/main/scala/monocle/std/Validation.scala
+++ core/src/main/scala/monocle/std/Validation.scala
@@ -1,6 +1,6 @@
 package monocle.std
 
-import monocle.{PIso, PPrism}
+import monocle.{Prism, Iso, PIso, PPrism}
 
 import scalaz.syntax.either._
 import scalaz.syntax.validation._
@@ -9,16 +9,25 @@ import scalaz.{Validation, \/}
 object validation extends ValidationFunctions
 
 trait ValidationFunctions {
-  final def success[E, A, B]: PPrism[Validation[E, A], Validation[E, B], A, B] =
+  final def pSuccess[E, A, B]: PPrism[Validation[E, A], Validation[E, B], A, B] =
     PPrism[Validation[E, A], Validation[E, B], A, B](
       _.fold(e => Validation.failure[E, B](e).left[A], a => a.right[Validation[E, B]])
     )(_.success[E])
 
-  final def failure[E, A, B]: PPrism[Validation[E, A], Validation[B, A], E, B] =
-    PPrism[Validation[E, A], Validation[B, A], E, B](
-      _.fold(e => e.right[Validation[B, A]], a => Validation.success[B, A](a).left[E])
+  final def success[E, A]: Prism[Validation[E, A], A] =
+    pSuccess[E, A, A]
+
+  final def pFailure[E, A, F]: PPrism[Validation[E, A], Validation[F, A], E, F] =
+    PPrism[Validation[E, A], Validation[F, A], E, F](
+      _.fold(e => e.right[Validation[F, A]], a => Validation.success[F, A](a).left[E])
     )(_.failure[A])
 
-  final def disjunctionIso[E1, E2, A1, A2]: PIso[Validation[E1, A1], Validation[E2, A2], E1 \/ A1, E2 \/ A2] =
+  final def failure[E, A]: Prism[Validation[E, A], E] =
+    pFailure[E, A, E]
+
+  final def pValidationToDisjunction[E1, E2, A1, A2]: PIso[Validation[E1, A1], Validation[E2, A2], E1 \/ A1, E2 \/ A2] =
     PIso[Validation[E1, A1], Validation[E2, A2], E1 \/ A1, E2 \/ A2](_.disjunction)(_.validation)
+
+  final def validationToDisjunction[E, A]: Iso[Validation[E, A], E \/ A] =
+    pValidationToDisjunction[E, E, A, A]
 }
--- test/src/test/scala/monocle/std/ValidationSpec.scala
+++ test/src/test/scala/monocle/std/ValidationSpec.scala
@@ -4,7 +4,7 @@ import monocle.MonocleSuite
 import monocle.law.discipline.{IsoTests, PrismTests}
 
 class ValidationSpec extends MonocleSuite {
-  checkAll("Validation is isomorphic to Disjunction", IsoTests(monocle.std.validation.disjunctionIso[String, String, Int, Int]))
-  checkAll("success", PrismTests(monocle.std.validation.success[Int, String, String]))
-  checkAll("failure", PrismTests(monocle.std.validation.failure[Int, String, Int]))
+  checkAll("Validation is isomorphic to Disjunction", IsoTests(monocle.std.validation.validationToDisjunction[String, Int]))
+  checkAll("success", PrismTests(monocle.std.validation.success[String, Int]))
+  checkAll("failure", PrismTests(monocle.std.validation.failure[String, Int]))
 }
