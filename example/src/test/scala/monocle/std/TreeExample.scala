95c37761c1c2c8aadbfee306489f5469d3a3e9a5
Brian McKenna <brian@brianmckenna.org>
2015-11-13 08:27:26 +1100

    Add a Plated test to scalaz.Tree example

--- example/src/test/scala/monocle/std/TreeExample.scala
+++ example/src/test/scala/monocle/std/TreeExample.scala
@@ -1,6 +1,6 @@
 package monocle.std
 
-import monocle.MonocleSuite
+import monocle.{MonocleSuite, Plated}
 
 import scalaz.Tree._
 
@@ -36,6 +36,10 @@ class TreeExample extends MonocleSuite {
     (tree    applyLens rightMostLabel get) shouldEqual 3
 
     (tree    applyLens rightMostLabel set 0) shouldEqual node(1, Stream(leaf(2), leaf(0)))
+  }
+
+  test("Plated universe gives us a stream of all node") {
+    Plated.universe(tree) shouldEqual Stream(tree, leaf(2), leaf(3))
   }
 
 }
