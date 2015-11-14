e0c9a7c5295c5c77e469b94d627cf232a3af5871
Brian McKenna <brian@brianmckenna.org>
2015-11-14 18:46:43 +1100

    Add ScalaDoc for Plated

--- core/src/main/scala/monocle/Plated.scala
+++ core/src/main/scala/monocle/Plated.scala
@@ -1,5 +1,6 @@
 package monocle
 
+import scala.annotation.implicitNotFound
 import scalaz._
 import scalaz.std.stream._
 
@@ -9,21 +10,39 @@ import scalaz.std.stream._
   *
   * @tparam A the parent and child type of a [[Plated]]
   */
+@implicitNotFound("Could not find an instance of Plated[{A}], please check Monocle instance location policy to " +
+  "find out which import is necessary")
 abstract class Plated[A] extends Serializable { self =>
   def plate: Traversal[A, A]
 }
 
 object Plated {
 
+  /** [[Traversal]] of immediate self-similar children */
   def plate[A](implicit P: Plated[A]): Traversal[A, A] = P.plate
+
+  /** get the immediate self-similar children of a target */
   @inline def children[A: Plated](a: A): List[A] = plate[A].getAll(a)
+
+  /** get all transitive self-similar elements of a target, including itself */
   def universe[A: Plated](a: A): Stream[A] = {
     val fold = plate[A].asFold
     def go(b: A): Stream[A] = b #:: fold.foldMap[Stream[A]](go)(b)
     go(a)
   }
+
+  /**
+    * rewrite a target by applying a rule as often as possible until it reaches
+    * a fixpoint (this is an infinite loop if there is no fixpoint)
+    */
   def rewrite[A: Plated](f: A => Option[A])(a: A): A =
     rewriteOf(plate[A].asSetter)(f)(a)
+
+  /**
+    * rewrite a target by applying a rule within a [[Setter]], as often as
+    * possible until it reaches a fixpoint (this is an infinite loop if there is
+    * no fixpoint)
+    */
   def rewriteOf[A](l: Setter[A, A])(f: A => Option[A])(a: A): A = {
     def go(b: A): A = {
       val c = transformOf(l)(go)(b)
@@ -31,8 +50,12 @@ object Plated {
     }
     go(a)
   }
+
+  /** transform every element */
   def transform[A: Plated](f: A => A)(a: A): A =
     transformOf(plate[A].asSetter)(f)(a)
+
+  /** transform every element by applying a [[Setter]] */
   def transformOf[A](l: Setter[A, A])(f: A => A)(a: A): A =
     l.modify(b => transformOf(l)(f)(f(b)))(a)
 
