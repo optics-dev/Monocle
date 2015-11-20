70329bc380d2d6ccdde34810c15f229f87eb041f
David Barri <japgolly@gmail.com>
2015-11-21 10:21:44 +1100

    Add GenIso.fields

--- macro/src/main/scala/monocle/macros/GenIso.scala
+++ macro/src/main/scala/monocle/macros/GenIso.scala
@@ -3,32 +3,44 @@ package monocle.macros
 import monocle.Iso
 
 import scala.reflect.internal.SymbolTable
-import scala.reflect.macros.blackbox
+import scala.reflect.macros.{blackbox, whitebox}
 
 object GenIso {
-  /** generate an [[Iso]] between a case class `S` and its unique field of type `A` */
+  /** Generate an [[Iso]] between a case class `S` and its unique field of type `A`. */
   def apply[S, A]: Iso[S, A] = macro GenIsoImpl.genIso_impl[S, A]
 
-  /** generate an [[Iso]] between an object `S` and Unit */
+  /** Generate an [[Iso]] between an object `S` and `Unit`. */
   def unit[S]: Iso[S, Unit] = macro GenIsoImpl.genIso_unit_impl[S]
+
+  /**
+    * Generate an [[Iso]] between a case class `S` and its fields.
+    *
+    * Case classes with 0 fields will correspond with `Unit`, 1 with the field type, 2 or more with
+    * a tuple of all field types in the same order as the fields themselves.
+    *
+    * Case classes with multiple parameter-lists (example: `case class X(…)(…)`) are rejected.
+    */
+  def fields[S]: Iso[S, _] = macro GenIsoImplW.genIso_fields_impl[S]
 }
 
 @macrocompat.bundle
 class GenIsoImpl(val c: blackbox.Context) {
-  private def caseAccessorsOf[S: c.WeakTypeTag]: List[c.universe.MethodSymbol] = {
-    import c.universe._
+  import c.universe._
+
+  protected final def fail(msg: String): Nothing =
+    c.abort(c.enclosingPosition, msg)
+
+  protected def caseAccessorsOf[S: c.WeakTypeTag]: List[MethodSymbol] = {
     weakTypeOf[S].decls.collect { case m: MethodSymbol if m.isCaseAccessor => m }.toList
   }
 
   def genIso_impl[S: c.WeakTypeTag, A: c.WeakTypeTag]: c.Expr[Iso[S, A]] = {
-    import c.universe._
-
     val (sTpe, aTpe) = (weakTypeOf[S], weakTypeOf[A])
 
     val fieldMethod = caseAccessorsOf[S] match {
       case m :: Nil => m
-      case Nil      => c.abort(c.enclosingPosition, s"Cannot find a case class accessor for $sTpe, $sTpe needs to be a case class with a single accessor")
-      case _        => c.abort(c.enclosingPosition, s"Found several case class accessor for $sTpe, $sTpe needs to be a case class with a single accessor")
+      case Nil      => fail(s"Cannot find a case class accessor for $sTpe, $sTpe needs to be a case class with a single accessor.")
+      case _        => fail(s"Found several case class accessor for $sTpe, $sTpe needs to be a case class with a single accessor.")
     }
 
     val sTpeSym = sTpe.typeSymbol.companion
@@ -59,27 +71,86 @@ class GenIsoImpl(val c: blackbox.Context) {
     """)
   }
 
-  def genIso_unit_impl[S: c.WeakTypeTag]: c.Expr[Iso[S, Unit]] = {
-    import c.universe._
-
+  protected def genIso_unit_tree[S: c.WeakTypeTag]: c.Tree = {
     val sTpe = weakTypeOf[S]
 
     if (sTpe.typeSymbol.isModuleClass) {
       val table = c.universe.asInstanceOf[SymbolTable]
       val tree = table.gen
-      val obj = tree.mkAttributedQualifier(sTpe.asInstanceOf[tree.global.Type]).asInstanceOf[c.universe.Tree]
-      c.Expr[Iso[S, Unit]](q"""
+      val obj = tree.mkAttributedQualifier(sTpe.asInstanceOf[tree.global.Type]).asInstanceOf[Tree]
+      q"""
         monocle.Iso[${sTpe}, Unit](Function.const(()))(Function.const(${obj}))
-      """)
+      """
     } else {
       caseAccessorsOf[S] match {
         case Nil =>
           val sTpeSym = sTpe.typeSymbol.companion
-          c.Expr[Iso[S, Unit]](q"""
+          q"""
             monocle.Iso[${sTpe}, Unit](Function.const(()))(Function.const(${sTpeSym}()))
-          """)
-        case _   => c.abort(c.enclosingPosition, s"$sTpe needs to be a case class with no accessor or an object.")
+          """
+        case _   => fail(s"$sTpe needs to be a case class with no accessor or an object.")
       }
+    }
+  }
+
+  def genIso_unit_impl[S: c.WeakTypeTag]: c.Expr[Iso[S, Unit]] =
+    c.Expr[Iso[S, Unit]](genIso_unit_tree[S])
+}
+
+@macrocompat.bundle
+class GenIsoImplW(override val c: whitebox.Context) extends GenIsoImpl(c) {
+  import c.universe._
+
+  protected final def nameAndType(T: Type, s: Symbol): (TermName, Type) = {
+    def paramType(name: TermName): Type =
+      T.decl(name).typeSignatureIn(T) match {
+        case NullaryMethodType(t) => t
+        case t                    => t
+      }
+    val a = s.asTerm.name
+    val A = paramType(a)
+    (a, A)
+  }
+
+  def genIso_fields_impl[S: c.WeakTypeTag]: c.Tree = {
+    val sTpe = weakTypeOf[S]
+
+    val sTpeSym = sTpe.typeSymbol.asClass
+    if (!sTpeSym.isCaseClass)
+      fail(s"$sTpe is not a case class.")
+
+    val paramLists = sTpe
+      .decls
+      .collectFirst { case m: MethodSymbol if m.isPrimaryConstructor => m }
+      .getOrElse(fail(s"Unable to discern primary constructor for $sTpe."))
+      .paramLists
+
+    paramLists match {
+      case Nil | Nil :: Nil =>
+        genIso_unit_tree[S]
+
+      case (param :: Nil) :: Nil =>
+        val (pName, pType) = nameAndType(sTpe, param)
+        q"""
+          monocle.Iso[$sTpe, $pType](_.$pName)(${sTpeSym.companion}(_))
+        """
+
+      case params :: Nil =>
+        var readField = List.empty[Tree]
+        var readTuple = List.empty[Tree]
+        var types = List.empty[Type]
+        for ((param, i) <- params.zipWithIndex.reverse) {
+          val (pName, pType) = nameAndType(sTpe, param)
+          readField ::= q"s.$pName"
+          readTuple ::= q"t.${TermName("_" + (i + 1))}"
+          types ::= pType
+        }
+        q"""
+          monocle.Iso[$sTpe, (..$types)](s => (..$readField))(t => ${sTpeSym.companion}(..$readTuple))
+        """
+
+      case _ :: _ :: _ =>
+        fail(s"Found several parameter-lists for $sTpe, $sTpe needs to be a case class with a single parameter-list.")
     }
   }
 }
--- test/src/test/scala/other/MacroOutSideMonocleSpec.scala
+++ test/src/test/scala/other/MacroOutSideMonocleSpec.scala
@@ -1,33 +1,44 @@
 package other
 
-import monocle.MonocleSuite
+import monocle.{Iso, MonocleSuite}
 import monocle.law.discipline.{IsoTests, LensTests, PrismTests}
 import monocle.macros.{GenIso, GenLens, GenPrism}
-import org.scalacheck.Arbitrary._
+import org.scalacheck.Arbitrary.{arbOption => _, _}
 import org.scalacheck.{Arbitrary, Gen}
 
 import scalaz.Equal
+import scalaz.std.option._
 
 class MacroOutSideMonocleSpec extends MonocleSuite {
 
   case class Example(i: Int)
+  case class Example2(l: Long, s: String)
   case object ExampleObject
   case class EmptyCase()
   case class EmptyCaseType[A]()
+  case class ExampleType[A](as: Option[A])
+  case class Example2Type[A](a: A, as: Option[A])
 
   sealed trait Foo
   case class Bar1(s: String) extends Foo
   case class Bar2(i: Int) extends Foo
 
   implicit val exampleArb: Arbitrary[Example] = Arbitrary(arbitrary[Int].map(Example.apply))
+  implicit val example2Arb: Arbitrary[Example2] = Arbitrary(for {l <- arbitrary[Long]; s <- arbitrary[String]} yield Example2(l, s))
   implicit val exampleObjArb: Arbitrary[ExampleObject.type] = Arbitrary(Gen.const(ExampleObject))
   implicit val emptyCaseArb: Arbitrary[EmptyCase] = Arbitrary(Gen.const(EmptyCase()))
   implicit def emptyCaseTypeArb[A]: Arbitrary[EmptyCaseType[A]] = Arbitrary(Gen.const(EmptyCaseType()))
   implicit val bar1Arb: Arbitrary[Bar1] = Arbitrary(arbitrary[String].map(Bar1.apply))
   implicit val bar2Arb: Arbitrary[Bar2] = Arbitrary(arbitrary[Int].map(Bar2.apply))
   implicit val fooArb: Arbitrary[Foo] = Arbitrary(Gen.oneOf(arbitrary[Bar1], arbitrary[Bar2]))
+  implicit def exampleTypeArb[A: Arbitrary]: Arbitrary[ExampleType[A]] = Arbitrary(Arbitrary.arbOption[A].arbitrary map ExampleType.apply)
+  implicit def example2TypeArb[A: Arbitrary]: Arbitrary[Example2Type[A]] =
+    Arbitrary(for {x <- arbitrary[A]; y <- Arbitrary.arbOption[A].arbitrary} yield Example2Type(x, y))
 
   implicit val exampleEq: Equal[Example] = Equal.equalA[Example]
+  implicit val example2Eq: Equal[Example2] = Equal.equalA[Example2]
+  implicit def exampleTypeEq[A](implicit as: Equal[Option[A]]): Equal[ExampleType[A]] = as.contramap(_.as)
+  implicit def example2TypeEq[A](implicit a: Equal[A], as: Equal[Option[A]]): Equal[Example2Type[A]] = Equal.equal((x, y) => a.equal(x.a, y.a) && as.equal(x.as, y.as))
   implicit val exampleObjEq: Equal[ExampleObject.type] = Equal.equalA[ExampleObject.type]
   implicit val emptyCaseEq: Equal[EmptyCase] = Equal.equalA[EmptyCase]
   implicit def emptyCaseTypeEq[A]: Equal[EmptyCaseType[A]] = Equal.equalA[EmptyCaseType[A]]
@@ -40,4 +51,21 @@ class MacroOutSideMonocleSpec extends MonocleSuite {
   checkAll("GenIso.unit empty case class with type param" , IsoTests(GenIso.unit[EmptyCaseType[Int]]))
   checkAll("GenLens"                                      , LensTests(GenLens[Example](_.i)))
   checkAll("GenPrism"                                     , PrismTests(GenPrism[Foo, Bar1]))
+
+
+  def testGenIsoFields[S: Arbitrary : Equal, A: Arbitrary : Equal](name: String, iso: Iso[S, A]) =
+    new FieldsTester(name, iso)
+
+  class FieldsTester[S: Arbitrary : Equal, A: Arbitrary : Equal](name: String, iso: Iso[S, A]) {
+    def expect[Expect](implicit ev: A =:= Expect) =
+      checkAll("GenIso.fields " + name, IsoTests(iso))
+  }
+
+  testGenIsoFields("empty case class", GenIso.fields[EmptyCase]).expect[Unit]
+  testGenIsoFields("case class 1",     GenIso.fields[Example]  ).expect[Int]
+  testGenIsoFields("case class 2",     GenIso.fields[Example2] ).expect[(Long, String)]
+
+  testGenIsoFields("empty case class with type param", GenIso.fields[EmptyCaseType[Int]]).expect[Unit]
+  testGenIsoFields("case class 1 with type param",     GenIso.fields[ExampleType[Int]]  ).expect[Option[Int]]
+  testGenIsoFields("case class 2 with type param",     GenIso.fields[Example2Type[Int]] ).expect[(Int, Option[Int])]
 }
