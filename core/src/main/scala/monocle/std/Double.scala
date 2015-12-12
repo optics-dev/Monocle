f1c3932f9904ead189b27ab541c7a87d4fc37342
Mikael Valot <mikael.valot@gmail.com>
2015-12-12 16:41:39 +0000

    doubleToFloat

--- core/src/main/scala/monocle/std/Double.scala
+++ core/src/main/scala/monocle/std/Double.scala
@@ -10,6 +10,6 @@ trait DoubleOptics {
     Prism[Double, Int](d => if(d.isValidInt) Some(d.toInt) else None)(_.toDouble)
 
   val doubleToFloat: Prism[Double, Float] =
-    Prism[Double, Float](d => if(d.isValidInt) Some(d.toInt) else None)(_.toDouble)
+    Prism[Double, Float]{d => val f = d.toFloat; if (f.toDouble == d) Some(f) else None}(_.toDouble)
 
 }
--- test/src/test/scala/monocle/std/DoubleSpec.scala
+++ test/src/test/scala/monocle/std/DoubleSpec.scala
@@ -4,5 +4,6 @@ import monocle.MonocleSuite
 import monocle.law.discipline.PrismTests
 
 class DoubleSpec extends MonocleSuite {
-  checkAll("Double to Int", PrismTests(doubleToInt))
+  checkAll("Double to Int"  , PrismTests(doubleToInt))
+  checkAll("Double to Float", PrismTests(doubleToFloat))
 }
