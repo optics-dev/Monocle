f26c523239164a9f982daf66232c4e6c3ef01cf4
Aldo Stracquadanio <aldo.strac@gmail.com>
2016-04-09 16:10:53 +0100

    Introduce generic Each derivation

--- test/shared/src/test/scala/monocle/generic/ProductSpec.scala
+++ test/shared/src/test/scala/monocle/generic/ProductSpec.scala
@@ -2,6 +2,7 @@ package monocle.generic
 
 import monocle.MonocleSuite
 import monocle.law.discipline.IsoTests
+import monocle.law.discipline.function.EachTests
 import org.scalacheck.Arbitrary
 
 import scalaz.Equal
@@ -11,10 +12,23 @@ class ProductSpec extends MonocleSuite {
   case class Person(name: String, age: Int)
 
   implicit val personEq: Equal[Person] = Equal.equalA
-  implicit val personArb: Arbitrary[Person] = Arbitrary(for{
+  implicit val personArb: Arbitrary[Person] = Arbitrary(for {
     n <- Arbitrary.arbitrary[String]
     a <- Arbitrary.arbitrary[Int]
   } yield Person(n, a))
 
+  case class Permissions(read: Boolean, write: Boolean, execute: Boolean)
+
+  implicit val nameEq: Equal[Permissions] = Equal.equalA
+  implicit val nameArb: Arbitrary[Permissions] = Arbitrary(for {
+    f <- Arbitrary.arbitrary[Boolean]
+    l <- Arbitrary.arbitrary[Boolean]
+    i <- Arbitrary.arbitrary[Boolean]
+  } yield Permissions(f, l, i))
+
   checkAll("toTuple", IsoTests(product.productToTuple[Person]))
+
+  checkAll("eachTuple2", EachTests[(String, String), String])
+  checkAll("eachTuple4", EachTests[(Int, Int, Int, Int), Int])
+  checkAll("eachCaseClass", EachTests[Permissions, Boolean])
 }
