33757032e185c317a7aed62241bf716222a1a3be
Brian McKenna <bmckenna@atlassian.com>
2016-06-10 08:50:12 +1000

    Remove explicit types for wrapped in examples
    I thought they were needed there, but they are not!

--- example/src/test/scala/monocle/function/WrappedExample.scala
+++ example/src/test/scala/monocle/function/WrappedExample.scala
@@ -12,11 +12,11 @@ import scalaz.syntax.foldable._
 class WrappedExample extends MonocleSuite {
 
   test("wrapped is an Iso") {
-    (Tags.Max(100) applyIso wrapped[Int @@ Tags.Max, Int] get) shouldEqual 100
+    (Tags.Max(100) applyIso wrapped get) shouldEqual 100
   }
 
   test("unwrapped is an Iso") {
-    ("Hello" applyIso unwrapped[String @@ Tags.Dual, String] get) shouldEqual Tags.Dual("Hello")
+    ("Hello" applyIso unwrapped get) shouldEqual Tags.Dual("Hello")
   }
 
   test("ala wraps and unwraps inside of a functor") {
