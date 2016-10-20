8e9511331ae8e3edb1fc7b82fa5fa0b820ea45f2
aoiroaoino <aoiro.aoino+github@gmail.com>
2016-10-21 03:22:49 +0900

    use Free.roll

--- core/shared/src/main/scala/monocle/std/Free.scala
+++ core/shared/src/main/scala/monocle/std/Free.scala
@@ -13,10 +13,7 @@ trait FreeOptics {
     val plate: Traversal[Free[S, A], Free[S, A]] = new Traversal[Free[S, A], Free[S, A]] {
       def modifyF[F[_]: Applicative](f: Free[S, A] => F[Free[S, A]])(s: Free[S, A]): F[Free[S, A]] =
         s.resume.fold(
-          as => Applicative[F].map(Traverse[S].traverse(as)(f)) {
-            // Free.roll does not exist in 7.1
-            Free.liftF(_).flatMap(identity)
-          },
+          as => Applicative[F].map(Traverse[S].traverse(as)(f))(Free.roll),
           x => Applicative[F].point(Free.point(x))
         )
     }
