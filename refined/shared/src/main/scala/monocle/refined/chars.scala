50bd10133cce41b11ead3807bdfc27599c056fb5
sinduja <sinduja.gnanakulenthiran@crowdmix.me>
2016-12-13 21:47:32 +0000

    Add support for refined Char (lowercase and uppercase)

---
+++ refined/shared/src/main/scala/monocle/refined/chars.scala
@@ -0,0 +1,21 @@
+package monocle.refined
+
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.char.{LowerCase, UpperCase}
+import monocle._
+
+
+object chars extends CharsInstances
+
+trait CharsInstances {
+  implicit val lowerCase: Prism[Char, LowerCaseChar] = toCase[LowerCase](c => c.isLower)
+  implicit val upperCase: Prism[Char, UpperCaseChar] = toCase[UpperCase](c => c.isUpper)
+
+  private def toCase[P](p: Char => Boolean): Prism[Char, Refined[Char, P]] = {
+    Prism[Char, Refined[Char, P]] {
+      case char if p(char) => Some(Refined.unsafeApply(char))
+      case _ => None
+    } {_.get}
+  }
+}
+
