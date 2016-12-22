5fd694c2af57a8c5686dc0f7747cc00dabdc4eae
Julien Truffaut <truffaut.julien@gmail.com>
2016-12-22 11:28:08 +0000

    Change Possible from Prism to Optional and add link in website

--- core/shared/src/main/scala/monocle/function/Possible.scala
+++ core/shared/src/main/scala/monocle/function/Possible.scala
@@ -1,59 +1,62 @@
 package monocle.function
 
-import monocle.{Iso, Prism}
-import scalaz.{\/, Validation, Maybe}
-import scala.util.{Try, Success, Failure}
+import monocle.{Iso, Optional}
+
+import scala.util.Try
+import scalaz.{Maybe, Validation, \/}
 
 /**
- * Typeclass that defines a [[Prism]] from a monomorphic container `S` to a possible `A` value.
- * @tparam S source of the [[Prism]]
- * @tparam A target of the [[Prism]], `A` is supposed to be unique for a given `S`
+ * Typeclass that defines an [[Optional]] from a monomorphic container `S` to a possible value `A`.
+ * There must be at most one `A` in `S`.
+ * @tparam S source of the [[Optional]]
+ * @tparam A target of the [[Optional]], `A` is supposed to be unique for a given `S`
  */
 abstract class Possible[S, A] extends Serializable {
-  def possible: Prism[S, A]
+  def possible: Optional[S, A]
 }
 
 trait PossibleFunctions {
-  def possible[S, A](implicit ev: Possible[S, A]): Prism[S, A] = ev.possible
+  def possible[S, A](implicit ev: Possible[S, A]): Optional[S, A] = ev.possible
 }
 
 object Possible extends PossibleFunctions {
-  /** lift an instance of [[Optional]] using an [[Iso]] */
+  /** lift an instance of [[monocle.Optional]] using an [[Iso]] */
   def fromIso[S, A, B](iso: Iso[S, A])(implicit ev: Possible[A, B]): Possible[S, B] = new Possible[S, B] {
-    val possible: Prism[S, B] =
-      iso composePrism ev.possible
+    val possible: Optional[S, B] =
+      iso composeOptional ev.possible
   }
 
   /************************************************************************************************/
   /** Std instances                                                                               */
   /************************************************************************************************/
+
   implicit def optionPossible[A]: Possible[Option[A], A] = 
     new Possible[Option[A], A] { 
-      def possible = monocle.std.option.some
+      def possible = monocle.std.option.some.asOptional
     }
 
   implicit def eitherPossible[A,B]: Possible[Either[A,B], B] =
     new Possible[Either[A,B], B] { 
-      def possible = monocle.std.either.stdRight
+      def possible = monocle.std.either.stdRight.asOptional
     }
 
   implicit def maybePossible[A,B]: Possible[Maybe[A], A] =
     new Possible[Maybe[A], A] { 
-      def possible = monocle.std.maybe.just
+      def possible = monocle.std.maybe.just.asOptional
     }
 
   implicit def disjunctionPossible[A,B]: Possible[A \/ B, B] =
     new Possible[A \/ B, B] { 
-      def possible = monocle.std.disjunction.right
+      def possible = monocle.std.disjunction.right.asOptional
     }
 
   implicit def validationPossible[A,B]: Possible[Validation[A,B], B] =
     new Possible[Validation[A,B], B] { 
-      def possible = monocle.std.validation.success
+      def possible = monocle.std.validation.success.asOptional
     }
 
   implicit def tryPossible[A]: Possible[Try[A], A] =
     new Possible[Try[A], A] { 
-      def possible = monocle.std.utilTry.trySuccess
+      def possible = monocle.std.utilTry.trySuccess.asOptional
     }
 }
