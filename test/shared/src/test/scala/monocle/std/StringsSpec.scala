07d4a6619a1dbe9f8c718f796a8370097301f55b
Maciej Czerwiński <mjjc@users.noreply.github.com>
2017-01-02 21:16:52 +0100

    Refactor UUID generation

--- test/shared/src/test/scala/monocle/std/StringsSpec.scala
+++ test/shared/src/test/scala/monocle/std/StringsSpec.scala
@@ -1,29 +1,11 @@
 package monocle.std
 
-import java.util.UUID
-
 import monocle.MonocleSuite
 import monocle.function.Plated._
 import monocle.law.discipline.function._
 import monocle.law.discipline.{IsoTests, PrismTests, TraversalTests}
-import org.scalacheck.Arbitrary
-
-import scalaz.Equal
 
 class StringsSpec extends MonocleSuite {
-  implicit def arbitraryUUID: Arbitrary[UUID] = Arbitrary(UUID.randomUUID)
-
-  implicit def equalUUID: Equal[UUID] = new Equal[UUID] {
-    override def equal(a1: UUID, a2: UUID): Boolean = a1 == a2
-  }
-
-  implicit def arbitraryUUIDtoUUID: Arbitrary[UUID => UUID] = Arbitrary(
-    for (
-      a <- Arbitrary.arbLong.arbitrary;
-      b <- Arbitrary.arbLong.arbitrary
-    ) yield (x: UUID) => new UUID(a * x.getMostSignificantBits, b * x.getLeastSignificantBits)
-  )
-
   checkAll("stringToList", IsoTests(stringToList))
   checkAll("reverse String", ReverseTests[String])
   checkAll("empty String", EmptyTests[String])
