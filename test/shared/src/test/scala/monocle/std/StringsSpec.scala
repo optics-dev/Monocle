be3abadcf58314ac34352dd00071cd050ae1c335
Maciej Czerwiński <mjjc@users.noreply.github.com>
2017-01-02 19:48:31 +0100

    Add UUID Prism. Fixes #485

--- test/shared/src/test/scala/monocle/std/StringsSpec.scala
+++ test/shared/src/test/scala/monocle/std/StringsSpec.scala
@@ -1,11 +1,27 @@
 package monocle.std
 
+import java.util.UUID
+
 import monocle.MonocleSuite
 import monocle.function.Plated._
 import monocle.law.discipline.function._
 import monocle.law.discipline.{IsoTests, PrismTests, TraversalTests}
+import org.scalacheck.Arbitrary
+
+import scalaz.Equal
 
 class StringsSpec extends MonocleSuite {
+  implicit def arbitraryUUID: Arbitrary[UUID] = Arbitrary(UUID.randomUUID)
+
+  implicit def equalUUID: Equal[UUID] = (a1: UUID, a2: UUID) => a1 == a2
+
+  implicit def arbitraryUUIDtoUUID: Arbitrary[UUID => UUID] = Arbitrary(
+    for (
+      a <- Arbitrary.arbLong.arbitrary;
+      b <- Arbitrary.arbLong.arbitrary
+    ) yield (x: UUID) => new UUID(a * x.getMostSignificantBits, b * x.getLeastSignificantBits)
+  )
+
   checkAll("stringToList", IsoTests(stringToList))
   checkAll("reverse String", ReverseTests[String])
   checkAll("empty String", EmptyTests[String])
@@ -19,6 +35,7 @@ class StringsSpec extends MonocleSuite {
   checkAll("String to Byte"    , PrismTests(stringToByte))
   checkAll("String to Int"     , PrismTests(stringToInt))
   checkAll("String to Long"    , PrismTests(stringToLong))
+  checkAll("String to UUID"    , PrismTests(stringToUUID))
 
   checkAll("plated String", TraversalTests(plate[String]))
 
