6652c42b4e244e3a6c51a42a893abca71cec20c5
Marcin Matuszak <mrcmatuszak@users.noreply.github.com>
2017-01-02 13:09:31 +0100

    Add Support for refined endsWith #481 (#483)
    Add Support for refined endsWith

--- refined/shared/src/main/scala/monocle/refined/strings.scala
+++ refined/shared/src/main/scala/monocle/refined/strings.scala
@@ -1,17 +1,24 @@
 package monocle.refined
 
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.StartsWith
+import eu.timepit.refined.api.{Refined, Validate}
+import eu.timepit.refined.string.{EndsWith, StartsWith}
 import monocle._
 
 object strings extends StringsInstances
 
 trait StringsInstances {
 
-  def startsWith(prefix: String): Prism[String, StartsWithString[prefix.type]] = {
-    Prism.partial[String, Refined[String, StartsWith[prefix.type]]] {
-      case string if string.startsWith(prefix) => Refined.unsafeApply(string)
-    }{_.value}
-  }
+  def startsWith(prefix: String)(implicit v: Validate[String, StartsWith[prefix.type]]): Prism[String, StartsWithString[prefix.type]] =
+    refinedPrism[String, StartsWith[prefix.type]](prefix)
 
+  def endsWith(suffix: String)(implicit v: Validate[String, EndsWith[suffix.type]]): Prism[String, EndsWithString[suffix.type]] =
+    refinedPrism[String, EndsWith[suffix.type]](suffix)
+
+  private def refinedPrism[T, P](t: T)(implicit v: Validate[T, P]): Prism[T, T Refined P] = {
+    Prism.partial[T, Refined[T, P]] {
+      case tt if v.isValid(tt) => Refined.unsafeApply[T, P](tt)
+    } {
+      _.value
+    }
+  }
 }
