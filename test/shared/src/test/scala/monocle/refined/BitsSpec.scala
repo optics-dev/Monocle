d69f78959a6cc7955a7a1c3a49f4e27b0fc91b07
Frank S. Thomas <frank@timepit.eu>
2017-08-16 04:12:15 +0200

    Update to refined 0.8.2
    This also makes use of refined-scalacheck instances in tests.

--- test/shared/src/test/scala/monocle/refined/BitsSpec.scala
+++ test/shared/src/test/scala/monocle/refined/BitsSpec.scala
@@ -1,25 +1,11 @@
 package monocle.refined
 
-import eu.timepit.refined._
-import eu.timepit.refined.api.Refined
+import eu.timepit.refined.W
+import eu.timepit.refined.scalacheck.numeric._
 import monocle.MonocleSuite
 import monocle.law.discipline.function.AtTests
-import org.scalacheck.{Arbitrary, Gen}
 
 class BitsSpec extends MonocleSuite {
-  implicit val byteBitsArb: Arbitrary[ByteBits] = Arbitrary(
-    Gen.choose(0, 7).map(Refined.unsafeApply)
-  )
-  implicit val charBitsArb: Arbitrary[CharBits] = Arbitrary(
-    Gen.choose(0, 15).map(Refined.unsafeApply)
-  )
-  implicit val intBitsArb: Arbitrary[IntBits] = Arbitrary(
-    Gen.choose(0, 31).map(Refined.unsafeApply)
-  )
-  implicit val longBitsArb: Arbitrary[LongBits] = Arbitrary(
-    Gen.choose(0, 63).map(Refined.unsafeApply)
-  )
-
   checkAll("Byte at bit", AtTests[Byte, ZeroTo[W.`7`.T], Boolean])
   checkAll("Char at bit", AtTests[Char, ZeroTo[W.`15`.T], Boolean])
   checkAll("Int at bit", AtTests[Int, ZeroTo[W.`31`.T], Boolean])
