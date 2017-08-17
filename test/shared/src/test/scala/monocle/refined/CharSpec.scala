0a2f8a6a588b2ebda5be909dda9dd804b7d17497
Frank S. Thomas <f.thomas@gmx.de>
2017-08-17 15:01:17 +0200

    Update to refined 0.8.2 (#529)
    This also makes use of refined-scalacheck instances in tests.

--- test/shared/src/test/scala/monocle/refined/CharSpec.scala
+++ test/shared/src/test/scala/monocle/refined/CharSpec.scala
@@ -1,22 +1,13 @@
 package monocle.refined
 
-import eu.timepit.refined.api.Refined
+import eu.timepit.refined.scalacheck.char._
+import eu.timepit.refined.scalacheck.refTypeCogen
 import monocle._
 import monocle.law.discipline.PrismTests
-import org.scalacheck.{Arbitrary, Cogen, Gen}
 
 import scalaz.Equal
 
 class CharSpec extends MonocleSuite {
-  implicit val lowerCaseRefinedCharArb: Arbitrary[LowerCaseChar] =
-    Arbitrary(Gen.alphaLowerChar.map(Refined.unsafeApply))
-
-  implicit val upperCaseRefinedCharArb: Arbitrary[UpperCaseChar] =
-    Arbitrary(Gen.alphaUpperChar.map(Refined.unsafeApply))
-
-  implicit val lowerCaseCoGen: Cogen[LowerCaseChar] = Cogen[Char].contramap[LowerCaseChar](_.value)
-  implicit val upperCaseCoGen: Cogen[UpperCaseChar] = Cogen[Char].contramap[UpperCaseChar](_.value)
-
   implicit val eqLowerCase: Equal[LowerCaseChar] = Equal.equalA[LowerCaseChar]
   implicit val eqUpperCase: Equal[UpperCaseChar] = Equal.equalA[UpperCaseChar]
   implicit val eqChar: Equal[Char] = Equal.equalA[Char]
--- test/shared/src/test/scala/monocle/refined/StringsSpec.scala
+++ test/shared/src/test/scala/monocle/refined/StringsSpec.scala
@@ -1,18 +1,15 @@
 package monocle.refined
 
-import eu.timepit.refined._
+import eu.timepit.refined.W
+import eu.timepit.refined.scalacheck.refTypeCogen
 import eu.timepit.refined.scalacheck.string.{endsWithArbitrary, startsWithArbitrary}
 import monocle.MonocleSuite
 import monocle.law.discipline.PrismTests
-import org.scalacheck.Cogen
 
 import scalaz.Equal
 
 
 class StringsSpec extends MonocleSuite {
-
-  implicit val startsWithCoGen: Cogen[StartsWithString[W.`"hello"`.T]] = Cogen[String].contramap[StartsWithString[W.`"hello"`.T]](_.value)
-  implicit val endsWithCoGen: Cogen[EndsWithString[W.`"world"`.T]] = Cogen[String].contramap[EndsWithString[W.`"world"`.T]](_.value)
 
   implicit val eqStartsWith: Equal[StartsWithString[W.`"hello"`.T]] = Equal.equalA[StartsWithString[W.`"hello"`.T]]
   implicit val eqEndsWith: Equal[EndsWithString[W.`"world"`.T]] = Equal.equalA[EndsWithString[W.`"world"`.T]]
