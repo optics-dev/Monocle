2544a9d50b827a96dbe2a7468538171ccf6fbf46
Diogo Castro <diogo.filipe.acastro@gmail.com>
2018-07-09 09:36:58 +0100

    Added `coProductIso`/`coProductDisjunctionIso` and `GenericOptics` (#578)
    * Added `coProductIso`/`coProductDisjunctionIso` and `GenericOptics`
    
    * Added `coProductToEither` and `coProductToDisjunction`

--- generic/shared/src/main/scala/monocle/generic/CoProduct.scala
+++ generic/shared/src/main/scala/monocle/generic/CoProduct.scala
@@ -1,16 +1,98 @@
 package monocle.generic
 
-import monocle.Prism
-import shapeless.Coproduct
-import shapeless.ops.coproduct.{Inject, Selector}
+import monocle.{Iso, Prism}
+import monocle.generic.internal.{CoproductToDisjunction, DisjunctionToCoproduct}
+import shapeless.{Coproduct, Generic}
+import shapeless.ops.coproduct.{CoproductToEither, EitherToCoproduct, Inject, Selector}
+import scalaz.\/
 
 object coproduct extends CoProductInstances
 
 
 trait CoProductInstances {
-  
+
   def coProductPrism[C <: Coproduct, A](implicit evInject: Inject[C, A], evSelector: Selector[C, A]): Prism[C, A] =
     Prism[C, A](evSelector.apply(_))(evInject.apply)
-  
 
+  /** An isomorphism between a coproduct [[S]] and the sum of its parts.
+    *
+    * {{{
+    *   type ISB = Int :+: String :+: Boolean :+: CNil
+    *
+    *   val iso: Iso[ISB, Either[Int, Either[String, Boolean]]] = coProductEitherIso[ISB].apply
+    * }}}
+    */
+  def coProductEitherIso[S <: Coproduct]: GenCoProductEitherIso[S] = new GenCoProductEitherIso
+
+  class GenCoProductEitherIso[S <: Coproduct] {
+    def apply[L, R](implicit
+                    coproductToEither: CoproductToEither.Aux[S, Either[L, R]],
+                    eitherToCoproduct: EitherToCoproduct.Aux[L, R, S]
+                   ): Iso[S, Either[L, R]] =
+      Iso(coproductToEither.apply)(eitherToCoproduct.apply)
+  }
+
+
+  /** An isomorphism between a sum type [[S]] (e.g. a sealed trait) and the sum of its parts.
+    *
+    * {{{
+    *   sealed trait S
+    *   case class A(name: String) extends S
+    *   case class B(name: String) extends S
+    *   case class C(otherName: String) extends S
+    *
+    *   val iso: Iso[S, Either[A, Either[B, C]]] = coProductToEither[S].apply
+    * }}}
+    */
+  def coProductToEither[S]: GenCoProductToEither[S] = new GenCoProductToEither
+
+  class GenCoProductToEither[S] {
+    def apply[C <: Coproduct, L, R](implicit
+                                    ev: Generic.Aux[S, C],
+                                    coproductToEither: CoproductToEither.Aux[C, Either[L, R]],
+                                    eitherToCoproduct: EitherToCoproduct.Aux[L, R, C]
+                                   ): Iso[S, Either[L, R]] =
+      generic.toGeneric[S] composeIso coProductEitherIso.apply
+  }
+
+
+  /** An isomorphism between a coproduct [[S]] and the sum of its parts.
+    *
+    * {{{
+    *   type ISB = Int :+: String :+: Boolean :+: CNil
+    *
+    *   val iso: Iso[ISB, Int \/ (String \/ Boolean)] = coProductDisjunctionIso[ISB].apply
+    * }}}
+    */
+  def coProductDisjunctionIso[S <: Coproduct]: GenCoProductDisjunctionIso[S] = new GenCoProductDisjunctionIso
+
+  class GenCoProductDisjunctionIso[S <: Coproduct] {
+    def apply[L, R](implicit
+                    coproductToDisjunction: CoproductToDisjunction.Aux[S, L \/ R],
+                    disjunctionToCoproduct: DisjunctionToCoproduct.Aux[L, R, S]
+                   ): Iso[S, L \/ R] =
+      Iso(coproductToDisjunction.apply)(disjunctionToCoproduct.apply)
+  }
+
+  /** An isomorphism between a sum type [[S]] (e.g. a sealed trait) and the sum of its parts.
+    *
+    * {{{
+    *   sealed trait S
+    *   case class A(name: String) extends S
+    *   case class B(name: String) extends S
+    *   case class C(otherName: String) extends S
+    *
+    *   val iso: Iso[S, A \/ (B \/ C)] = coProductToDisjunction[S].apply
+    * }}}
+    */
+  def coProductToDisjunction[S]: GenCoProductToDisjunction[S] = new GenCoProductToDisjunction
+
+  class GenCoProductToDisjunction[S] {
+    def apply[C <: Coproduct, L, R](implicit
+                                    ev: Generic.Aux[S, C],
+                                    coproductToDisjunction: CoproductToDisjunction.Aux[C, L \/ R],
+                                    disjunctionToCoproduct: DisjunctionToCoproduct.Aux[L, R, C]
+                                   ): Iso[S, L \/ R] =
+      generic.toGeneric[S] composeIso coProductDisjunctionIso.apply
+  }
 }
--- test/shared/src/test/scala/monocle/generic/CoproductSpec.scala
+++ test/shared/src/test/scala/monocle/generic/CoproductSpec.scala
@@ -1,9 +1,9 @@
 package monocle.generic
 
 import monocle.MonocleSuite
-import monocle.law.discipline.PrismTests
+import monocle.law.discipline.{PrismTests, IsoTests}
 import org.scalacheck.Arbitrary._
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.{Arbitrary, Cogen, Gen}
 import shapeless.{:+:, CNil, Coproduct, Inl, Inr}
 
 import scalaz.Equal
@@ -27,4 +27,26 @@ class CoproductSpec extends MonocleSuite {
 
   checkAll("Coproduct Prism", PrismTests(coProductPrism[IB, Boolean]))
 
+  checkAll("Coproduct Either Iso", IsoTests(coProductEitherIso[IB].apply))
+
+  checkAll("Coproduct Disjunction Iso", IsoTests(coProductDisjunctionIso[IB].apply))
+
+  sealed trait S
+  case class A() extends S
+  case object B extends S
+
+  implicit val sArbitrary = Arbitrary[S](Gen.oneOf(A(), B))
+  implicit val sEqual = Equal.equal[S](_ == _)
+
+  implicit val aArb: Arbitrary[A] = Arbitrary(A())
+  implicit val aEq: Equal[A]      = Equal.equal(_ == _)
+  implicit val aCogen: Cogen[A]   = Cogen(_ => 1)
+
+  implicit val bArb: Arbitrary[B.type] = Arbitrary(B)
+  implicit val bEq: Equal[B.type]      = Equal.equal(_ == _)
+  implicit val bCogen: Cogen[B.type]   = Cogen(_ => 2)
+
+  checkAll("Coproduct To Either", IsoTests(coProductToEither[S].apply))
+
+  checkAll("Coproduct To Disjunction", IsoTests(coProductToDisjunction[S].apply))
 }
