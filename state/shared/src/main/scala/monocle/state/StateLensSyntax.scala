18ed39e7629d51e2065c21b96dade6ca1f52b699
Valy Diarrassouba <2129101+valydia@users.noreply.github.com>
2019-01-10 20:01:27 +0000

    Add missing function to State module (fixes #548) (#620)
    * Add missing function to State module (fixes #548)
    
    * Bump up rvm to fix travis-ci

--- state/shared/src/main/scala/monocle/state/StateLensSyntax.scala
+++ state/shared/src/main/scala/monocle/state/StateLensSyntax.scala
@@ -1,8 +1,8 @@
 package monocle.state
 
+import cats.{Eval, Now}
 import monocle.PLens
-
-import cats.data.State
+import cats.data.{IndexedStateT, State}
 
 trait StateLensSyntax {
   implicit def toStateLensOps[S, T, A, B](lens: PLens[S, T, A, B]): StateLensOps[S, T, A, B] =
@@ -25,4 +25,32 @@ final class StateLensOps[S, T, A, B](lens: PLens[S, T, A, B]) {
   /** extracts the value viewed through the lens and applies `f` over it */
   def extracts[B](f: A => B): State[S, B] =
     extract.map(f)
+
+  /** modify the value viewed through the lens and returns its *new* value */
+  def mod(f: A => B): IndexedStateT[Eval, S, T, B] =
+    IndexedStateT(s => {
+      val a = lens.get(s)
+      val b = f(a)
+      Now((lens.set(b)(s), b))
+    })
+
+  /** modify the value viewed through the lens and returns its *old* value */
+  def modo(f: A => B): IndexedStateT[Eval, S, T, A] =
+    toState.bimap(lens.modify(f), identity)
+
+  /** modify the value viewed through the lens and ignores both values */
+  def mod_(f: A => B): IndexedStateT[Eval, S, T, Unit] =
+    IndexedStateT(s => Now((lens.modify(f)(s), ())))
+
+  /** set the value viewed through the lens and returns its *new* value */
+  def assign(b: B): IndexedStateT[Eval, S, T, B] =
+    mod(_ => b)
+
+  /** set the value viewed through the lens and returns its *old* value */
+  def assigno(b: B): IndexedStateT[Eval, S, T, A] =
+    modo(_ => b)
+
+  /** set the value viewed through the lens and ignores both values */
+  def assign_(b: B): IndexedStateT[Eval, S, T, Unit] =
+    mod_(_ => b)
 }
--- state/shared/src/main/scala/monocle/state/StateOptionalSyntax.scala
+++ state/shared/src/main/scala/monocle/state/StateOptionalSyntax.scala
@@ -1,8 +1,8 @@
 package monocle.state
 
+import cats.{Eval, Now}
 import monocle.POptional
-
-import cats.data.State
+import cats.data.{IndexedStateT, State}
 
 trait StateOptionalSyntax {
   implicit def toStateOptionalOps[S, T, A, B](optional: POptional[S, T, A, B]): StateOptionalOps[S, T, A, B] =
@@ -25,4 +25,28 @@ final class StateOptionalOps[S, T, A, B](optional: POptional[S, T, A, B]) {
   /** extracts the value viewed through the optional and applies `f` over it */
   def extracts[B](f: A => B): State[S, Option[B]] =
     extract.map(_.map(f))
+
+  /** modify the value viewed through the Optional and return its *new* value, if there is one */
+  def mod(f: A => B): IndexedStateT[Eval, S, T, Option[B]] =
+    modo(f).map(_.map(f))
+
+  /** modify the value viewed through the Optional and return its *old* value, if there was one */
+  def modo(f: A => B): IndexedStateT[Eval, S, T, Option[A]] =
+    IndexedStateT(s => Now((optional.modify(f)(s), optional.getOption(s))))
+
+  /** modify the value viewed through the Optional and ignores both values */
+  def mod_(f: A => B): IndexedStateT[Eval, S, T, Unit] =
+    IndexedStateT(s => Now((optional.modify(f)(s), ())))
+
+  /** set the value viewed through the Optional and returns its *new* value */
+  def assign(b: B): IndexedStateT[Eval, S, T, Option[B]] =
+    mod(_ => b)
+
+  /** set the value viewed through the Optional and return its *old* value, if there was one */
+  def assigno(b: B): IndexedStateT[Eval, S, T, Option[A]] =
+    modo(_ => b)
+
+  /** set the value viewed through the Optional and ignores both values */
+  def assign_(b: B): IndexedStateT[Eval,S, T, Unit] =
+    mod_(_ => b)
 }
