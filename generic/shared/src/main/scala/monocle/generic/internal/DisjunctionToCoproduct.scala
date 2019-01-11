214479eb99349cb522a124064b97e2075148fce5
maxkorolev <korolev.maksim.a@gmail.com>
2019-01-11 12:43:17 +0300

    Added `coProductIso`/`coProductDisjunctionIso` and `GenericOptics` (#578)
    * Added `coProductIso`/`coProductDisjunctionIso` and `GenericOptics`
    
    * Added `coProductToEither` and `coProductToDisjunction`

---
+++ generic/shared/src/main/scala/monocle/generic/internal/DisjunctionToCoproduct.scala
@@ -0,0 +1,37 @@
+package monocle.generic.internal
+
+import scala.{Either => \/}
+import shapeless._
+
+/**
+  * Typeclass converting an [[Either]] to a [[\/]].
+  *
+  * Based on [[shapeless.ops.coproduct.EitherToCoproduct]]:
+  * https://github.com/milessabin/shapeless/blob/shapeless-2.3.3-scala-2.13.0-M4/core/src/main/scala/shapeless/ops/coproduct.scala#L1305-L1335
+  */
+sealed trait DisjunctionToCoproduct[L, R] extends DepFn1[L \/ R] with Serializable { type Out <: Coproduct }
+
+object DisjunctionToCoproduct extends DisjunctionToCoproductLowPrio {
+  type Aux[L, R, Out0 <: Coproduct] = DisjunctionToCoproduct[L, R] { type Out = Out0 }
+
+  implicit def econsDisjunctionToCoproduct[L, RL, RR, Out0 <: Coproduct](implicit
+    evR: DisjunctionToCoproduct.Aux[RL, RR, Out0]
+  ): DisjunctionToCoproduct.Aux[L, RL \/ RR, L :+: Out0] = new DisjunctionToCoproduct[L, RL \/ RR] {
+    type Out = L :+: Out0
+    def apply(t: L \/ (RL \/ RR)): L :+: Out0 = t match {
+      case Left(l) => Inl(l)
+      case Right(r) => Inr(evR(r))
+    }
+  }
+}
+
+trait DisjunctionToCoproductLowPrio {
+  implicit def baseDisjunctionToCoproduct[L, R]: DisjunctionToCoproduct.Aux[L, R, L :+: R :+: CNil] = new DisjunctionToCoproduct[L, R] {
+    type Out = L :+: R :+: CNil
+
+    def apply(t: L \/ R): L :+: R :+: CNil = t match {
+      case Left(l) => Inl(l)
+      case Right(r) => Coproduct[L :+: R :+: CNil](r)
+    }
+  }
+}
