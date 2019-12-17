a881ff6c67aee74ad4f809d356a3606ef872f314
Julien Truffaut <truffaut.julien@gmail.com>
2019-12-17 16:28:42 +0000

    POC of Fold based on Iterator (#759)
    * POC of Fold based on iterator
    
    * replace fold by foldLeft
    
    * add map to Fold adn Getter

--- kernel/shared/src/main/scala/monocle/Getter.scala
+++ kernel/shared/src/main/scala/monocle/Getter.scala
@@ -1,7 +1,16 @@
 package monocle
 
-trait Getter[A, B] { self =>
+trait Getter[A, B] extends Fold[A, B] { self =>
   def get(from: A): B
+
+  final override def toIterator(from: A): Iterator[B] =
+    collection.Iterator.single(get(from))
+
+
+  final override def map[C](f: B => C): Getter[A, C] =
+    new Getter[A, C] {
+      def get(from: A): C = f(self.get(from))
+    }
 
   def compose[C](other: Getter[B, C]): Getter[A, C] =
     new Getter[A, C] {
