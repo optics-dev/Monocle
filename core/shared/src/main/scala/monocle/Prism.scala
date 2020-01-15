4ebf3df2dc1b095c7b1c34b923f13dda694f64d3
pascalh <pascal.hof@tu-dortmund.de>
2020-01-15 07:03:45 +0100

    Turned syntax case classes into regular classes

--- core/shared/src/main/scala/monocle/Prism.scala
+++ core/shared/src/main/scala/monocle/Prism.scala
@@ -318,7 +318,7 @@ sealed abstract class PrismInstances {
   }
 }
 
-final case class PrismSyntax[S, A](private val self: Prism[S, A]) extends AnyVal {
+final class PrismSyntax[S, A](private val self: Prism[S, A]) extends AnyVal {
 
   /** lift a [[Prism]] such as it only matches if all elements of `F[S]` are getOrModify */
   def below[F[_]](implicit F: Traverse[F]): Prism[F[S], F[A]] =
