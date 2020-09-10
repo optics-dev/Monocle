28cc3ec41fd17b9375c9abe397589bde9fc8162d
Thomas Dufour <chwthewke@users.noreply.github.com>
2020-09-10 15:49:01 +0200

    Add POptionals into Ior's left and right (#893)
    theseLeft[A, B, C]: POptional[Ior[A, B], Ior[C, B], A, C]
    and
    theseRight[A, B, C]: POptional[Ior[A, B], Ior[A, C], B, C]
    
    can look into the left and right parts
    of an Ior and modify any present value.

--- core/shared/src/main/scala/monocle/std/These.scala
+++ core/shared/src/main/scala/monocle/std/These.scala
@@ -1,5 +1,7 @@
 package monocle.std
 
+import monocle.Optional
+import monocle.POptional
 import monocle.Prism
 
 import cats.data.Ior.{Both, Right => That, Left => This}
@@ -18,6 +20,20 @@ trait TheseOptics {
       case Left(a)  => This(a)
       case Right(b) => That(b)
     }
+
+  def pTheseLeft[A, B, C]: POptional[Ior[A, B], Ior[C, B], A, C] =
+    POptional[Ior[A, B], Ior[C, B], A, C](ior => ior.fold(a => Right(a), b => Left(Ior.right(b)), (a, _) => Right(a)))(
+      c => ior => ior.leftMap(_ => c)
+    )
+
+  def theseLeft[A, B]: Optional[Ior[A, B], A] = pTheseLeft[A, B, A]
+
+  def pTheseRight[A, B, C]: POptional[Ior[A, B], Ior[A, C], B, C] =
+    POptional[Ior[A, B], Ior[A, C], B, C](ior => ior.fold(a => Left(Ior.left(a)), b => Right(b), (_, b) => Right(b)))(
+      c => ior => ior.map(_ => c)
+    )
+
+  def theseRight[A, B]: Optional[Ior[A, B], B] = pTheseRight[A, B, B]
 
   @deprecated("use theseToDisjunction", since = "1.2.0")
   def theseDisjunction[A, B]: Prism[Ior[A, B], Either[A, B]] =
--- test/shared/src/test/scala/monocle/std/TheseSpec.scala
+++ test/shared/src/test/scala/monocle/std/TheseSpec.scala
@@ -1,10 +1,15 @@
 package monocle.std
 
 import monocle.MonocleSuite
+import monocle.law.discipline.OptionalTests
 import monocle.law.discipline.PrismTests
 
 class TheseSpec extends MonocleSuite {
   import cats.laws.discipline.arbitrary._
 
   checkAll("These - Disjunction", PrismTests(theseToDisjunction[Int, String]))
+
+  checkAll("These - Left", OptionalTests(theseLeft[Int, String]))
+
+  checkAll("These - Right", OptionalTests(theseRight[Int, String]))
 }
