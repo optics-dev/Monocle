02bd3c6f1ecb28d1f9bf88830671b181b95728eb
Bartłomiej Wierciński <bmwbartek1@gmail.com>
2020-10-01 11:30:49 +0200

    add extra notes in @Lenses documentation and Lens page (#903)
    Co-authored-by: Bartłomiej Wierciński <b.wiercinski@ocado.com>
    Co-authored-by: Julien Truffaut <truffaut.julien@gmail.com>

--- macro/src/main/scala/monocle/macros/Lenses.scala
+++ macro/src/main/scala/monocle/macros/Lenses.scala
@@ -2,10 +2,18 @@ package monocle.macros
 
 import scala.reflect.macros.blackbox
 
+/**
+  * Note: before using `@Lenses` remember to activate macro annotations.
+  * See "Getting started" section in the library documentation.
+  */
 class Lenses(prefix: String = "") extends scala.annotation.StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro LensesImpl.lensesAnnotationMacro
 }
 
+/**
+  * Note: before using `@PLenses` remember to activate macro annotations.
+  * See "Getting started" section in the library documentation.
+  */
 class PLenses(prefix: String = "") extends scala.annotation.StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro LensesImpl.plensesAnnotationMacro
 }
