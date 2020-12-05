b79bb379e0015625c58c374ae3b3190d983dd7fc
Bogdan <gohcbro@gmail.com>
2020-12-05 18:58:00 +0300

    Revert "Move extension methods for ApplyFold and ApplyGetter to syntax objects"
    This reverts commit 58f6532ea325b623c5daf5459a430de0d04c8705.

--- core/shared/src/main/scala/monocle/syntax/ApplyGetter.scala
+++ core/shared/src/main/scala/monocle/syntax/ApplyGetter.scala
@@ -9,10 +9,22 @@ final case class ApplyGetter[S, A](s: S, getter: Getter[S, A]) {
   @inline def exist(p: A => Boolean): S => Boolean  = getter.exist(p)
   @inline def find(p: A => Boolean): S => Option[A] = getter.find(p)
 
+  def each[C](implicit evEach: Each[A, C]): ApplyFold[S, C] =
+    composeTraversal(evEach.each)
+
   def some[A1](implicit ev1: A =:= Option[A1]): ApplyFold[S, A1] =
     adapt[Option[A1]] composePrism (std.option.pSome)
 
-  private[monocle] def adapt[A1](implicit evA: A =:= A1): ApplyGetter[S, A1] =
+  def withDefault[A1: Eq](defaultValue: A1)(implicit ev1: A =:= Option[A1]): ApplyGetter[S, A1] =
+    adapt[Option[A1]] composeIso (std.option.withDefault(defaultValue))
+
+  def at[I, A1](i: I)(implicit evAt: At[A, i.type, A1]): ApplyGetter[S, A1] =
+    composeLens(evAt.at(i))
+
+  def index[I, A1](i: I)(implicit evIndex: Index[A, I, A1]): ApplyFold[S, A1] =
+    composeOptional(evIndex.index(i))
+
+  private def adapt[A1](implicit evA: A =:= A1): ApplyGetter[S, A1] =
     evA.substituteCo[ApplyGetter[S, *]](this)
 
   def andThen[B](other: Fold[A, B]): ApplyFold[S, B] =
@@ -43,23 +55,4 @@ final case class ApplyGetter[S, A](s: S, getter: Getter[S, A]) {
 
   /** alias to composeIso */
   @inline def ^<->[B, C, D](other: PIso[A, B, C, D]): ApplyGetter[S, C] = andThen(other)
-}
-
-object ApplyGetter {
-  implicit def applyGetterSyntax[S, A](self: ApplyGetter[S, A]): ApplyGetterSyntax[S, A] =
-    new ApplyGetterSyntax(self)
-}
-
-final case class ApplyGetterSyntax[S, A](self: ApplyGetter[S, A]) extends AnyVal {
-  def each[C](implicit evEach: Each[A, C]): ApplyFold[S, C] =
-    self composeTraversal evEach.each
-
-  def withDefault[A1: Eq](defaultValue: A1)(implicit ev1: A =:= Option[A1]): ApplyGetter[S, A1] =
-    self.adapt[Option[A1]] composeIso std.option.withDefault(defaultValue)
-
-  def at[I, A1](i: I)(implicit evAt: At[A, i.type, A1]): ApplyGetter[S, A1] =
-    self composeLens evAt.at(i)
-
-  def index[I, A1](i: I)(implicit evIndex: Index[A, I, A1]): ApplyFold[S, A1] =
-    self composeOptional evIndex.index(i)
 }
