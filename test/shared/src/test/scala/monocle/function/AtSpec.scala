f46f5d9d2345dcedc45af0755404375e6f9ad568
Julien Truffaut <julien@fp-tower.com>
2020-12-24 20:54:43 +0000

    deprecate all applyX methods and operators in favor of optics method

--- test/shared/src/test/scala/monocle/function/AtSpec.scala
+++ test/shared/src/test/scala/monocle/function/AtSpec.scala
@@ -18,19 +18,19 @@ class AtSpec extends MonocleSuite {
 
   test("at creates a Lens from a Map, SortedMap to an optional value") {
     val map = Map("One" -> 1, "Two" -> 2)
-    assertEquals((map applyLens at("One")).replace(Some(-1)), Map("One" -> -1, "Two" -> 2))
-    assertEquals((map applyLens at("Two")).get, Some(2))
+    assertEquals(map.optics.at("One").replace(Some(-1)), Map("One" -> -1, "Two" -> 2))
+    assertEquals(map.optics.at("Two").get, Some(2))
   }
 
   test("at for tuples") {
     val tuple2 = (true, "hello")
     val tuple3 = (true, "hello", 5)
 
-    assertEquals((tuple2 applyLens at(1)).get, true)
-    assertEquals((tuple2 applyLens at(2)).get, "hello")
+    assertEquals(tuple2.optics.at(1).get, true)
+    assertEquals(tuple2.optics.at(2).get, "hello")
 
-    assertEquals((tuple3 applyLens at(1)).get, true)
-    assertEquals((tuple3 applyLens at(2)).get, "hello")
-    assertEquals((tuple3 applyLens at(3)).get, 5)
+    assertEquals(tuple3.optics.at(1).get, true)
+    assertEquals(tuple3.optics.at(2).get, "hello")
+    assertEquals(tuple3.optics.at(3).get, 5)
   }
 }
