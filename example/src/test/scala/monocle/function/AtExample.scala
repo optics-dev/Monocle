a3a70453da666a35de19a1ca2bd23c0a6440bb2a
Julien Truffaut <truffaut.julien@gmail.com>
2020-12-25 14:57:22 +0000

    deprecate all applyX methods and operators in favor of optics method (#1003)

--- example/src/test/scala/monocle/function/AtExample.scala
+++ example/src/test/scala/monocle/function/AtExample.scala
@@ -9,17 +9,17 @@ import scala.collection.immutable.SortedMap
 
 class AtExample extends MonocleSuite {
   test("at creates a Lens from a Map, SortedMap to an optional value") {
-    assertEquals((Map("One" -> 2, "Two" -> 2) applyLens at("Two") get), Some(2))
-    assertEquals((SortedMap("One" -> 2, "Two" -> 2) applyLens at("Two") get), Some(2))
+    assertEquals(Map("One" -> 2, "Two" -> 2).optics.at("Two").get, Some(2))
+    assertEquals(SortedMap("One" -> 2, "Two" -> 2).optics.at("Two").get, Some(2))
 
-    assertEquals((Map("One" -> 1, "Two" -> 2) applyLens at("One") replace Some(-1)), Map("One" -> -1, "Two" -> 2))
+    assertEquals(Map("One" -> 1, "Two" -> 2).optics.at("One").replace(Some(-1)), Map("One" -> -1, "Two" -> 2))
 
     // can delete a value
-    assertEquals((Map("One" -> 1, "Two" -> 2) applyLens at("Two") replace None), Map("One" -> 1))
+    assertEquals(Map("One" -> 1, "Two" -> 2).optics.at("Two").replace(None), Map("One" -> 1))
 
     // add a new value
     assertEquals(
-      (Map("One" -> 1, "Two" -> 2) applyLens at("Three") replace Some(3)),
+      Map("One" -> 1, "Two" -> 2).optics.at("Three").replace(Some(3)),
       Map(
         "One"   -> 1,
         "Two"   -> 2,
@@ -29,27 +29,27 @@ class AtExample extends MonocleSuite {
   }
 
   test("at creates a Lens from a Set to an optional element of the Set") {
-    assertEquals((Set(1, 2, 3) applyLens at(2) get), true)
-    assertEquals((Set(1, 2, 3) applyLens at(4) get), false)
+    assertEquals(Set(1, 2, 3).optics.at(2).get, true)
+    assertEquals(Set(1, 2, 3).optics.at(4).get, false)
 
-    assertEquals((Set(1, 2, 3) applyLens at(4) replace true), Set(1, 2, 3, 4))
-    assertEquals((Set(1, 2, 3) applyLens at(2) replace false), Set(1, 3))
+    assertEquals(Set(1, 2, 3).optics.at(4).replace(true), Set(1, 2, 3, 4))
+    assertEquals(Set(1, 2, 3).optics.at(2).replace(false), Set(1, 3))
   }
 
   test("at creates a Lens from Int to one of its bit") {
-    assertEquals((3 applyLens at(0: IntBits) get), true)  // true  means bit is 1
-    assertEquals((4 applyLens at(0: IntBits) get), false) // false means bit is 0
+    assertEquals(3.optics.at(0: IntBits).get, true)  // true  means bit is 1
+    assertEquals(4.optics.at(0: IntBits).get, false) // false means bit is 0
 
-    assertEquals((32 applyLens at(0: IntBits) replace true), 33)
-    assertEquals((3 applyLens at(1: IntBits) modify (!_)), 1) // toggle 2nd bit
+    assertEquals(32.optics.at(0: IntBits).replace(true), 33)
+    assertEquals(3.optics.at(1: IntBits).modify(!_), 1) // toggle 2nd bit
 
     illTyped("""0 applyLens at(79: IntBits) get""", "Right predicate.*fail.*")
     illTyped("""0 applyLens at(-1: IntBits) get""", "Left predicate.*fail.*")
   }
 
   test("at creates a Lens from Char to one of its bit") {
-    assertEquals(('x' applyLens at(0: CharBits) get), false)
-    assertEquals(('x' applyLens at(0: CharBits) replace true), 'y')
+    assertEquals('x'.optics.at(0: CharBits).get, false)
+    assertEquals('x'.optics.at(0: CharBits).replace(true), 'y')
   }
 
   test("remove deletes an element of a Map") {
--- test/shared/src/test/scala/monocle/GetterSpec.scala
+++ test/shared/src/test/scala/monocle/GetterSpec.scala
@@ -82,7 +82,7 @@ class GetterSpec extends MonocleSuite {
     val getter = Getter((_: SomeTest).y)
 
     assertEquals(getter.some.getAll(obj), List(2))
-    assertEquals(obj.applyGetter(getter).some.getAll, List(2))
+    assertEquals(obj.optics.andThen(getter).some.getAll, List(2))
   }
 
   test("withDefault") {
@@ -95,8 +95,8 @@ class GetterSpec extends MonocleSuite {
     assertEquals(getter.withDefault(0).get(objSome), 2)
     assertEquals(getter.withDefault(0).get(objNone), 0)
 
-    assertEquals(objSome.applyGetter(getter).withDefault(0).get, 2)
-    assertEquals(objNone.applyGetter(getter).withDefault(0).get, 0)
+    assertEquals(objSome.optics.andThen(getter).withDefault(0).get, 2)
+    assertEquals(objNone.optics.andThen(getter).withDefault(0).get, 0)
   }
 
   test("each") {
@@ -106,7 +106,7 @@ class GetterSpec extends MonocleSuite {
     val getter = Getter((_: SomeTest).y)
 
     assertEquals(getter.each.getAll(obj), List(1, 2, 3))
-    assertEquals(obj.applyGetter(getter).each.getAll, List(1, 2, 3))
+    assertEquals(obj.optics.andThen(getter).each.getAll, List(1, 2, 3))
   }
 
   test("filter") {
@@ -116,7 +116,7 @@ class GetterSpec extends MonocleSuite {
     val getter = Getter[SomeTest, Int](_.y)
 
     assertEquals(getter.filter(_ > 0).getAll(obj), List(2))
-    assertEquals(obj.applyGetter(getter).filter(_ > 0).getAll, List(2))
+    assertEquals(obj.optics.andThen(getter).filter(_ > 0).getAll, List(2))
   }
 
   test("filterIndex") {
@@ -126,7 +126,7 @@ class GetterSpec extends MonocleSuite {
     val getter = Getter[SomeTest, List[String]](_.y)
 
     assertEquals(getter.filterIndex((_: Int) > 0).getAll(obj), List("world"))
-    assertEquals(obj.applyGetter(getter).filterIndex((_: Int) > 0).getAll, List("world"))
+    assertEquals(obj.optics.andThen(getter).filterIndex((_: Int) > 0).getAll, List("world"))
   }
 
   test("at") {
@@ -134,17 +134,17 @@ class GetterSpec extends MonocleSuite {
     val tuple2Getter = Getter.id[(Int, Int)]
     assertEquals(tuple2Getter.at(1).get(tuple2), 1)
     assertEquals(tuple2Getter.at(2).get(tuple2), 2)
-    assertEquals(tuple2.applyGetter(tuple2Getter).at(1).get, 1)
-    assertEquals(tuple2.applyGetter(tuple2Getter).at(2).get, 2)
+    assertEquals(tuple2.optics.andThen(tuple2Getter).at(1).get, 1)
+    assertEquals(tuple2.optics.andThen(tuple2Getter).at(2).get, 2)
 
     val tuple3       = (1, 2, 3)
     val tuple3Getter = Getter.id[(Int, Int, Int)]
     assertEquals(tuple3Getter.at(1).get(tuple3), 1)
     assertEquals(tuple3Getter.at(2).get(tuple3), 2)
     assertEquals(tuple3Getter.at(3).get(tuple3), 3)
-    assertEquals(tuple3.applyGetter(tuple3Getter).at(1).get, 1)
-    assertEquals(tuple3.applyGetter(tuple3Getter).at(2).get, 2)
-    assertEquals(tuple3.applyGetter(tuple3Getter).at(3).get, 3)
+    assertEquals(tuple3.optics.andThen(tuple3Getter).at(1).get, 1)
+    assertEquals(tuple3.optics.andThen(tuple3Getter).at(2).get, 2)
+    assertEquals(tuple3.optics.andThen(tuple3Getter).at(3).get, 3)
 
     val tuple4       = (1, 2, 3, 4)
     val tuple4Getter = Getter.id[(Int, Int, Int, Int)]
@@ -152,10 +152,10 @@ class GetterSpec extends MonocleSuite {
     assertEquals(tuple4Getter.at(2).get(tuple4), 2)
     assertEquals(tuple4Getter.at(3).get(tuple4), 3)
     assertEquals(tuple4Getter.at(4).get(tuple4), 4)
-    assertEquals(tuple4.applyGetter(tuple4Getter).at(1).get, 1)
-    assertEquals(tuple4.applyGetter(tuple4Getter).at(2).get, 2)
-    assertEquals(tuple4.applyGetter(tuple4Getter).at(3).get, 3)
-    assertEquals(tuple4.applyGetter(tuple4Getter).at(4).get, 4)
+    assertEquals(tuple4.optics.andThen(tuple4Getter).at(1).get, 1)
+    assertEquals(tuple4.optics.andThen(tuple4Getter).at(2).get, 2)
+    assertEquals(tuple4.optics.andThen(tuple4Getter).at(3).get, 3)
+    assertEquals(tuple4.optics.andThen(tuple4Getter).at(4).get, 4)
 
     val tuple5       = (1, 2, 3, 4, 5)
     val tuple5Getter = Getter.id[(Int, Int, Int, Int, Int)]
@@ -164,11 +164,11 @@ class GetterSpec extends MonocleSuite {
     assertEquals(tuple5Getter.at(3).get(tuple5), 3)
     assertEquals(tuple5Getter.at(4).get(tuple5), 4)
     assertEquals(tuple5Getter.at(5).get(tuple5), 5)
-    assertEquals(tuple5.applyGetter(tuple5Getter).at(1).get, 1)
-    assertEquals(tuple5.applyGetter(tuple5Getter).at(2).get, 2)
-    assertEquals(tuple5.applyGetter(tuple5Getter).at(3).get, 3)
-    assertEquals(tuple5.applyGetter(tuple5Getter).at(4).get, 4)
-    assertEquals(tuple5.applyGetter(tuple5Getter).at(5).get, 5)
+    assertEquals(tuple5.optics.andThen(tuple5Getter).at(1).get, 1)
+    assertEquals(tuple5.optics.andThen(tuple5Getter).at(2).get, 2)
+    assertEquals(tuple5.optics.andThen(tuple5Getter).at(3).get, 3)
+    assertEquals(tuple5.optics.andThen(tuple5Getter).at(4).get, 4)
+    assertEquals(tuple5.optics.andThen(tuple5Getter).at(5).get, 5)
 
     val tuple6       = (1, 2, 3, 4, 5, 6)
     val tuple6Getter = Getter.id[(Int, Int, Int, Int, Int, Int)]
@@ -178,40 +178,40 @@ class GetterSpec extends MonocleSuite {
     assertEquals(tuple6Getter.at(4).get(tuple6), 4)
     assertEquals(tuple6Getter.at(5).get(tuple6), 5)
     assertEquals(tuple6Getter.at(6).get(tuple6), 6)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(1).get, 1)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(2).get, 2)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(3).get, 3)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(4).get, 4)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(5).get, 5)
-    assertEquals(tuple6.applyGetter(tuple6Getter).at(6).get, 6)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(1).get, 1)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(2).get, 2)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(3).get, 3)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(4).get, 4)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(5).get, 5)
+    assertEquals(tuple6.optics.andThen(tuple6Getter).at(6).get, 6)
 
     val sortedMap       = immutable.SortedMap(1 -> "one")
     val sortedMapGetter = Getter.id[immutable.SortedMap[Int, String]]
     assertEquals(sortedMapGetter.at(1).get(sortedMap), Some("one"))
     assertEquals(sortedMapGetter.at(0).get(sortedMap), None)
-    assertEquals(sortedMap.applyGetter(sortedMapGetter).at(1).get, Some("one"))
-    assertEquals(sortedMap.applyGetter(sortedMapGetter).at(0).get, None)
+    assertEquals(sortedMap.optics.andThen(sortedMapGetter).at(1).get, Some("one"))
+    assertEquals(sortedMap.optics.andThen(sortedMapGetter).at(0).get, None)
 
     val listMap       = immutable.ListMap(1 -> "one")
     val listMapGetter = Getter.id[immutable.ListMap[Int, String]]
     assertEquals(listMapGetter.at(1).get(listMap), Some("one"))
     assertEquals(listMapGetter.at(0).get(listMap), None)
-    assertEquals(listMap.applyGetter(listMapGetter).at(1).get, Some("one"))
-    assertEquals(listMap.applyGetter(listMapGetter).at(0).get, None)
+    assertEquals(listMap.optics.andThen(listMapGetter).at(1).get, Some("one"))
+    assertEquals(listMap.optics.andThen(listMapGetter).at(0).get, None)
 
     val map       = immutable.Map(1 -> "one")
     val mapGetter = Getter.id[Map[Int, String]]
     assertEquals(mapGetter.at(1).get(map), Some("one"))
     assertEquals(mapGetter.at(0).get(map), None)
-    assertEquals(map.applyGetter(mapGetter).at(1).get, Some("one"))
-    assertEquals(map.applyGetter(mapGetter).at(0).get, None)
+    assertEquals(map.optics.andThen(mapGetter).at(1).get, Some("one"))
+    assertEquals(map.optics.andThen(mapGetter).at(0).get, None)
 
     val set       = Set(1)
     val setGetter = Getter.id[Set[Int]]
     assertEquals(setGetter.at(1).get(set), true)
     assertEquals(setGetter.at(0).get(set), false)
-    assertEquals(set.applyGetter(setGetter).at(1).get, true)
-    assertEquals(set.applyGetter(setGetter).at(0).get, false)
+    assertEquals(set.optics.andThen(setGetter).at(1).get, true)
+    assertEquals(set.optics.andThen(setGetter).at(0).get, false)
   }
 
   test("index") {
@@ -219,70 +219,70 @@ class GetterSpec extends MonocleSuite {
     val listGetter = Getter.id[List[Int]]
     assertEquals(listGetter.index(0).getAll(list), List(1))
     assertEquals(listGetter.index(1).getAll(list), Nil)
-    assertEquals(list.applyGetter(listGetter).index(0).getAll, List(1))
-    assertEquals(list.applyGetter(listGetter).index(1).getAll, Nil)
+    assertEquals(list.optics.andThen(listGetter).index(0).getAll, List(1))
+    assertEquals(list.optics.andThen(listGetter).index(1).getAll, Nil)
 
     val lazyList       = LazyList(1)
     val lazyListGetter = Getter.id[LazyList[Int]]
     assertEquals(lazyListGetter.index(0).getAll(lazyList), List(1))
     assertEquals(lazyListGetter.index(1).getAll(lazyList), Nil)
-    assertEquals(lazyList.applyGetter(lazyListGetter).index(0).getAll, List(1))
-    assertEquals(lazyList.applyGetter(lazyListGetter).index(1).getAll, Nil)
+    assertEquals(lazyList.optics.andThen(lazyListGetter).index(0).getAll, List(1))
+    assertEquals(lazyList.optics.andThen(lazyListGetter).index(1).getAll, Nil)
 
     val listMap       = immutable.ListMap(1 -> "one")
     val listMapGetter = Getter.id[immutable.ListMap[Int, String]]
     assertEquals(listMapGetter.index(0).getAll(listMap), Nil)
     assertEquals(listMapGetter.index(1).getAll(listMap), List("one"))
-    assertEquals(listMap.applyGetter(listMapGetter).index(0).getAll, Nil)
-    assertEquals(listMap.applyGetter(listMapGetter).index(1).getAll, List("one"))
+    assertEquals(listMap.optics.andThen(listMapGetter).index(0).getAll, Nil)
+    assertEquals(listMap.optics.andThen(listMapGetter).index(1).getAll, List("one"))
 
     val map       = Map(1 -> "one")
     val mapGetter = Getter.id[Map[Int, String]]
     assertEquals(mapGetter.index(0).getAll(map), Nil)
     assertEquals(mapGetter.index(1).getAll(map), List("one"))
-    assertEquals(map.applyGetter(mapGetter).index(0).getAll, Nil)
-    assertEquals(map.applyGetter(mapGetter).index(1).getAll, List("one"))
+    assertEquals(map.optics.andThen(mapGetter).index(0).getAll, Nil)
+    assertEquals(map.optics.andThen(mapGetter).index(1).getAll, List("one"))
 
     val sortedMap       = immutable.SortedMap(1 -> "one")
     val sortedMapGetter = Getter.id[immutable.SortedMap[Int, String]]
     assertEquals(sortedMapGetter.index(0).getAll(sortedMap), Nil)
     assertEquals(sortedMapGetter.index(1).getAll(sortedMap), List("one"))
-    assertEquals(sortedMap.applyGetter(sortedMapGetter).index(0).getAll, Nil)
-    assertEquals(sortedMap.applyGetter(sortedMapGetter).index(1).getAll, List("one"))
+    assertEquals(sortedMap.optics.andThen(sortedMapGetter).index(0).getAll, Nil)
+    assertEquals(sortedMap.optics.andThen(sortedMapGetter).index(1).getAll, List("one"))
 
     val vector       = Vector(1)
     val vectorGetter = Getter.id[Vector[Int]]
     assertEquals(vectorGetter.index(0).getAll(vector), List(1))
     assertEquals(vectorGetter.index(1).getAll(vector), Nil)
-    assertEquals(vector.applyGetter(vectorGetter).index(0).getAll, List(1))
-    assertEquals(vector.applyGetter(vectorGetter).index(1).getAll, Nil)
+    assertEquals(vector.optics.andThen(vectorGetter).index(0).getAll, List(1))
+    assertEquals(vector.optics.andThen(vectorGetter).index(1).getAll, Nil)
 
     val chain       = Chain.one(1)
     val chainGetter = Getter.id[Chain[Int]]
     assertEquals(chainGetter.index(0).getAll(chain), List(1))
     assertEquals(chainGetter.index(1).getAll(chain), Nil)
-    assertEquals(chain.applyGetter(chainGetter).index(0).getAll, List(1))
-    assertEquals(chain.applyGetter(chainGetter).index(1).getAll, Nil)
+    assertEquals(chain.optics.andThen(chainGetter).index(0).getAll, List(1))
+    assertEquals(chain.optics.andThen(chainGetter).index(1).getAll, Nil)
 
     val nec       = NonEmptyChain.one(1)
     val necGetter = Getter.id[NonEmptyChain[Int]]
     assertEquals(necGetter.index(0).getAll(nec), List(1))
     assertEquals(necGetter.index(1).getAll(nec), Nil)
-    assertEquals(nec.applyGetter(necGetter).index(0).getAll, List(1))
-    assertEquals(nec.applyGetter(necGetter).index(1).getAll, Nil)
+    assertEquals(nec.optics.andThen(necGetter).index(0).getAll, List(1))
+    assertEquals(nec.optics.andThen(necGetter).index(1).getAll, Nil)
 
     val nev       = NonEmptyVector.one(1)
     val nevGetter = Getter.id[NonEmptyVector[Int]]
     assertEquals(nevGetter.index(0).getAll(nev), List(1))
     assertEquals(nevGetter.index(1).getAll(nev), Nil)
-    assertEquals(nev.applyGetter(nevGetter).index(0).getAll, List(1))
-    assertEquals(nev.applyGetter(nevGetter).index(1).getAll, Nil)
+    assertEquals(nev.optics.andThen(nevGetter).index(0).getAll, List(1))
+    assertEquals(nev.optics.andThen(nevGetter).index(1).getAll, Nil)
 
     val nel       = NonEmptyList.one(1)
     val nelGetter = Getter.id[NonEmptyList[Int]]
     assertEquals(nelGetter.index(0).getAll(nel), List(1))
     assertEquals(nelGetter.index(1).getAll(nel), Nil)
-    assertEquals(nel.applyGetter(nelGetter).index(0).getAll, List(1))
-    assertEquals(nel.applyGetter(nelGetter).index(1).getAll, Nil)
+    assertEquals(nel.optics.andThen(nelGetter).index(0).getAll, List(1))
+    assertEquals(nel.optics.andThen(nelGetter).index(1).getAll, Nil)
   }
 }
