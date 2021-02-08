f7ee107828baf4838b5f3d0ec0a5aa3f46d7ee9b
Julien Truffaut <julien@fp-tower.com>
2021-02-08 16:38:27 +0000

    remove extra empty line

--- core/shared/src/main/scala-3.x/monocle/Focus.scala
+++ core/shared/src/main/scala-3.x/monocle/Focus.scala
@@ -6,8 +6,7 @@ object Focus {
 
   extension [A] (opt: Option[A])
     def some: A = scala.sys.error("Extension method 'some' should only be used within the moocle.Focus macro.")
-
-
+  
   def apply[S] = new MkFocus[S]
 
   class MkFocus[S] {
