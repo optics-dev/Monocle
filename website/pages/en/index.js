6aa33e6ac66faf9906c2278f2790578f2d38b21d
Julien Truffaut <julien@fp-tower.com>
2021-02-17 13:44:23 +0000

    format and remove space

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -99,13 +99,13 @@ Monocle is a Scala library which offers a simple yet powerful API to access and 
 \`\`\`scala
 import monocle.syntax.all._
 
-val user = User( "Anna", Address(12, "high street"))
+val user = User("Anna", Address(12, "high street"))
 
 user.focus(_.name).replace("Bob")
-// res: User = User( "Bob", Address(12, "high street"))
+// res: User = User("Bob", Address(12, "high street"))
 
 user.focus(_.address.streetName).modify(_.toUpperCase)
-// res: User = User( "Anna", Address(12, "HIGH STREET"))
+// res: User = User("Anna", Address(12, "HIGH STREET"))
 
 user.focus(_.address.streetNumber).get
 // res: Int = 12
