0b330bf4acafadffb9686c84444934cd4c8159c6
Julien Truffaut <julien@fp-tower.com>
2021-02-23 21:18:58 +0000

    remove at tuple test

--- core/shared/src/test/scala-3.x/monocle/focus/FocusAtTest.scala
+++ core/shared/src/test/scala-3.x/monocle/focus/FocusAtTest.scala
@@ -21,15 +21,6 @@ final class FocusAtTest extends munit.FunSuite {
     assertEquals(atB.replace(None)(map), map - "b")
   }
 
-  test("Get/set on tuples") {
-    val at3 = Focus[(Int,Int,Int)](_.at(3))
-
-    val tup = (2, 3, 4)
-
-    assertEquals(at3.get(tup), 4)
-    assertEquals(at3.replace(55)(tup), (2, 3, 55))
-  }
-
   test("Focus operator `at` commutes with standalone operator `at`") {
     type PeopleMap = Map[String,Int]
     val map: PeopleMap = Map("Bob" -> 44, "Sue" -> 21, "Etienne" -> 33)
