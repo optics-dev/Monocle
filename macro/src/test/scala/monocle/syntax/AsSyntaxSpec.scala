a05a564b7eaadfde749e4c665b4235931f4281e9
Julien Truffaut <julien@fp-tower.com>
2021-03-12 12:43:57 +0000

    add as syntax for AppliedOptics in Scala 2

--- macro/src/test/scala/monocle/syntax/AsSyntaxSpec.scala
+++ macro/src/test/scala/monocle/syntax/AsSyntaxSpec.scala
@@ -8,7 +8,7 @@ class AsSyntaxSpec extends DisciplineSuite {
 
   sealed trait StringOrInt
   case class S(value: String) extends StringOrInt
-  case class I(value: Int) extends StringOrInt
+  case class I(value: Int)    extends StringOrInt
 
   val iso: Iso[StringOrInt, StringOrInt]             = Iso.id
   val lens: Lens[StringOrInt, StringOrInt]           = Iso.id
@@ -19,16 +19,16 @@ class AsSyntaxSpec extends DisciplineSuite {
   val getter: Getter[StringOrInt, StringOrInt]       = Iso.id
   val fold: Fold[StringOrInt, StringOrInt]           = Iso.id
 
-  test("iso.as"){ assertEquals(iso.as[I].getOption(I(1)), Some(I(1))) }
-  test("lens.as"){ assertEquals(lens.as[I].getOption(I(1)), Some(I(1))) }
-  test("prism.as"){ assertEquals(prism.as[I].getOption(I(1)), Some(I(1))) }
-  test("optional.as"){ assertEquals(optional.as[I].getOption(I(1)), Some(I(1))) }
-  test("traversal.as"){ assertEquals(traversal.as[I].getAll(I(1)), List(I(1))) }
-  test("setter.as"){ assertEquals(setter.as[I].replace(I(5))(I(1)), I(5)) }
-  test("getter.as"){ assert(getter.as[I].getAll(I(1)) == List(I(1))) }
-  test("fold.as"){ assert(fold.as[I].getAll(I(1)) == List(I(1))) }
+  test("iso.as")(assertEquals(iso.as[I].getOption(I(1)), Some(I(1))))
+  test("lens.as")(assertEquals(lens.as[I].getOption(I(1)), Some(I(1))))
+  test("prism.as")(assertEquals(prism.as[I].getOption(I(1)), Some(I(1))))
+  test("optional.as")(assertEquals(optional.as[I].getOption(I(1)), Some(I(1))))
+  test("traversal.as")(assertEquals(traversal.as[I].getAll(I(1)), List(I(1))))
+  test("setter.as")(assertEquals(setter.as[I].replace(I(5))(I(1)), I(5)))
+  test("getter.as")(assert(getter.as[I].getAll(I(1)) == List(I(1))))
+  test("fold.as")(assert(fold.as[I].getAll(I(1)) == List(I(1))))
 
-  val sOrI: StringOrInt = I(1)
+  val sOrI: StringOrInt                                            = I(1)
   val appliedIso: AppliedIso[StringOrInt, StringOrInt]             = AppliedIso(sOrI, iso)
   val appliedLens: AppliedLens[StringOrInt, StringOrInt]           = AppliedIso(sOrI, iso)
   val appliedPrism: AppliedPrism[StringOrInt, StringOrInt]         = AppliedIso(sOrI, iso)
@@ -38,13 +38,13 @@ class AsSyntaxSpec extends DisciplineSuite {
   val appliedGetter: AppliedGetter[StringOrInt, StringOrInt]       = AppliedIso(sOrI, iso)
   val appliedFold: AppliedFold[StringOrInt, StringOrInt]           = AppliedIso(sOrI, iso)
 
-  test("applied iso.as"){ assertEquals(appliedIso.as[I].getOption, Some(I(1))) }
-  test("applied lens.as"){ assertEquals(appliedLens.as[I].getOption, Some(I(1))) }
-  test("applied prism.as"){ assertEquals(appliedPrism.as[I].getOption, Some(I(1))) }
-  test("applied optional.as"){ assertEquals(appliedOptional.as[I].getOption, Some(I(1))) }
-  test("applied traversal.as"){ assertEquals(appliedTraversal.as[I].getAll, List(I(1))) }
-  test("applied setter.as"){ assertEquals(appliedSetter.as[I].replace(I(5)), I(5)) }
-  test("applied getter.as"){ assert(appliedGetter.as[I].getAll == List(I(1))) }
-  test("applied fold.as"){ assert(appliedFold.as[I].getAll == List(I(1))) }
+  test("applied iso.as")(assertEquals(appliedIso.as[I].getOption, Some(I(1))))
+  test("applied lens.as")(assertEquals(appliedLens.as[I].getOption, Some(I(1))))
+  test("applied prism.as")(assertEquals(appliedPrism.as[I].getOption, Some(I(1))))
+  test("applied optional.as")(assertEquals(appliedOptional.as[I].getOption, Some(I(1))))
+  test("applied traversal.as")(assertEquals(appliedTraversal.as[I].getAll, List(I(1))))
+  test("applied setter.as")(assertEquals(appliedSetter.as[I].replace(I(5)), I(5)))
+  test("applied getter.as")(assert(appliedGetter.as[I].getAll == List(I(1))))
+  test("applied fold.as")(assert(appliedFold.as[I].getAll == List(I(1))))
 
 }
