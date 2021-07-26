ed17bdebced66af7ff0e14b95f654ec85c88b53f
Ken Scambler <ken.scambler@gmail.com>
2021-07-27 00:02:13 +1000

    Refactored most of the refocus code away

--- core/shared/src/main/scala-3.x/monocle/internal/focus/ComposedFocusImpl.scala
+++ core/shared/src/main/scala-3.x/monocle/internal/focus/ComposedFocusImpl.scala
@@ -4,37 +4,23 @@ import monocle.{Focus, Lens, Iso, Prism, Optional, Traversal, Getter, Setter, Fo
 import scala.quoted.{Type, Expr, Quotes, quotes}
 
 private[monocle] object ComposedFocusImpl {
-  def apply[S: Type, A: Type, Next: Type](optic: Expr[Setter[S,A] | Fold[S,A]], lambda: Expr[Focus.KeywordContext ?=> A => Next])(using Quotes): Expr[Any] = {
+
+  type AnyOptic[S,A] = Setter[S,A] | Fold[S,A] | AppliedSetter[S,A] | AppliedFold[S,A]
+
+  def apply[S: Type, A: Type, Next: Type](optic: Expr[AnyOptic[S,A]], lambda: Expr[Focus.KeywordContext ?=> A => Next])(using Quotes): Expr[Any] = {
     import quotes.reflect._
     
     val generatedOptic = FocusImpl(lambda).asTerm
     val opticType = optic.asTerm.tpe.widen
     val nextType = TypeRepr.of[Next]
-    
-    def onlyRequiresOneTypeParameter(tpe: TypeRepr): Boolean = 
-      tpe =:= TypeRepr.of[Fold[S,A]] || tpe =:= TypeRepr.of[Getter[S,A]]
+    val singleTypeParam: Boolean = 
+      opticType =:= TypeRepr.of[Fold[S,A]] || 
+      opticType =:= TypeRepr.of[Getter[S,A]] || 
+      opticType =:= TypeRepr.of[AppliedFold[S,A]] || 
+      opticType =:= TypeRepr.of[AppliedGetter[S,A]]
 
-    if (onlyRequiresOneTypeParameter(opticType)) {
-      Select.overloaded(optic.asTerm, "andThen", List(nextType), List(generatedOptic)).asExpr
-    } else {
-      Select.overloaded(optic.asTerm, "andThen", List(nextType, nextType), List(generatedOptic)).asExpr
-    }
-  }
+    val typeParams = if (singleTypeParam) List(nextType) else List(nextType, nextType)
 
-  def applied[S: Type, A: Type, Next: Type](optic: Expr[AppliedSetter[S,A] | AppliedFold[S,A]], lambda: Expr[Focus.KeywordContext ?=> A => Next])(using Quotes): Expr[Any] = {
-    import quotes.reflect._
-    
-    val generatedOptic = FocusImpl(lambda).asTerm
-    val opticType = optic.asTerm.tpe.widen
-    val nextType = TypeRepr.of[Next]
-
-    def onlyRequiresOneTypeParameter(tpe: TypeRepr): Boolean = 
-      tpe =:= TypeRepr.of[AppliedFold[S,A]] || tpe =:= TypeRepr.of[AppliedGetter[S,A]]
-
-    if (onlyRequiresOneTypeParameter(opticType)) {
-      Select.overloaded(optic.asTerm, "andThen", List(nextType), List(generatedOptic)).asExpr
-    } else {
-      Select.overloaded(optic.asTerm, "andThen", List(nextType, nextType), List(generatedOptic)).asExpr
-    }
+    Select.overloaded(optic.asTerm, "andThen", typeParams, List(generatedOptic)).asExpr
   }
 }
--- core/shared/src/main/scala-3.x/monocle/syntax/ComposedFocusSyntax.scala
+++ core/shared/src/main/scala-3.x/monocle/syntax/ComposedFocusSyntax.scala
@@ -5,13 +5,8 @@ import monocle.internal.focus.ComposedFocusImpl
 
 trait ComposedFocusSyntax {
 
-  extension [S, A, Next] (optic: Setter[S, A] | Fold[S,A]) {
+  extension [S, A, Next] (optic: Setter[S, A] | Fold[S,A] | AppliedSetter[S,A] | AppliedFold[S,A]) {
     transparent inline def refocus(inline lambda: (Focus.KeywordContext ?=> A => Next)): Any = 
       ${ComposedFocusImpl[S, A, Next]('optic, 'lambda)}
-  }
-
-  extension [S, A, Next] (optic: AppliedSetter[S, A] | AppliedFold[S,A]) {
-    transparent inline def refocus(inline lambda: (Focus.KeywordContext ?=> A => Next)): Any =
-      ${ComposedFocusImpl.applied[S, A, Next]('optic, 'lambda)}
   }
 }
