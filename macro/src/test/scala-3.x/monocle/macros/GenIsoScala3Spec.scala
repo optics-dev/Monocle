208b54e9ccee510dffae1b2e56a522272c09fff3
Scala Steward <me@scala-steward.org>
2021-08-27 21:56:21 +0200

    Reformat with scalafmt 3.0.1

--- macro/src/test/scala-3.x/monocle/macros/GenIsoScala3Spec.scala
+++ macro/src/test/scala-3.x/monocle/macros/GenIsoScala3Spec.scala
@@ -11,24 +11,24 @@ class GenIsoScala3Spec extends DisciplineSuite {
   import GenIsoSpec._
 
   implicit val genEmptyTupleF: Arbitrary[EmptyTuple => EmptyTuple] = Arbitrary(Gen.const(_ => EmptyTuple))
-  implicit val genEmptyTuple: Arbitrary[EmptyTuple] = Arbitrary(Gen.const(EmptyTuple))
-  implicit val eqEmptyTuple: Eq[EmptyTuple] = Eq.fromUniversalEquals
+  implicit val genEmptyTuple: Arbitrary[EmptyTuple]                = Arbitrary(Gen.const(EmptyTuple))
+  implicit val eqEmptyTuple: Eq[EmptyTuple]                        = Eq.fromUniversalEquals
 
   trait CompileTimeTests {
-    locally { type S = CaseObj.type    ; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
-    locally { type S = Zero            ; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
-    locally { type S = ZeroT[Int]      ; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
-    locally { type S = One             ; val x = GenIso.fieldsTuple[S]; x: Iso[S, Tuple1[Int]] }
-    locally { type S = OneT[Int]       ; val x = GenIso.fieldsTuple[S]; x: Iso[S, Tuple1[Int]] }
-    locally { type S = Two             ; val x = GenIso.fieldsTuple[S]; x: Iso[S, (Int, Short)] }
+    locally { type S = CaseObj.type; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
+    locally { type S = Zero; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
+    locally { type S = ZeroT[Int]; val x = GenIso.fieldsTuple[S]; x: Iso[S, EmptyTuple] }
+    locally { type S = One; val x = GenIso.fieldsTuple[S]; x: Iso[S, Tuple1[Int]] }
+    locally { type S = OneT[Int]; val x = GenIso.fieldsTuple[S]; x: Iso[S, Tuple1[Int]] }
+    locally { type S = Two; val x = GenIso.fieldsTuple[S]; x: Iso[S, (Int, Short)] }
     locally { type S = TwoT[Int, Short]; val x = GenIso.fieldsTuple[S]; x: Iso[S, (Int, Short)] }
   }
 
-  checkAll("GenIso.fieldsTuple[CaseObj.type]"    , IsoTests(GenIso.fieldsTuple[CaseObj.type]))
-  checkAll("GenIso.fieldsTuple[Zero]"            , IsoTests(GenIso.fieldsTuple[Zero]))
-  checkAll("GenIso.fieldsTuple[ZeroT[Int]]"      , IsoTests(GenIso.fieldsTuple[ZeroT[Int]]))
-  checkAll("GenIso.fieldsTuple[One]"             , IsoTests(GenIso.fieldsTuple[One]))
-  checkAll("GenIso.fieldsTuple[OneT[Int]]"       , IsoTests(GenIso.fieldsTuple[OneT[Int]]))
-  checkAll("GenIso.fieldsTuple[Two]"             , IsoTests(GenIso.fieldsTuple[Two]))
+  checkAll("GenIso.fieldsTuple[CaseObj.type]", IsoTests(GenIso.fieldsTuple[CaseObj.type]))
+  checkAll("GenIso.fieldsTuple[Zero]", IsoTests(GenIso.fieldsTuple[Zero]))
+  checkAll("GenIso.fieldsTuple[ZeroT[Int]]", IsoTests(GenIso.fieldsTuple[ZeroT[Int]]))
+  checkAll("GenIso.fieldsTuple[One]", IsoTests(GenIso.fieldsTuple[One]))
+  checkAll("GenIso.fieldsTuple[OneT[Int]]", IsoTests(GenIso.fieldsTuple[OneT[Int]]))
+  checkAll("GenIso.fieldsTuple[Two]", IsoTests(GenIso.fieldsTuple[Two]))
   checkAll("GenIso.fieldsTuple[TwoT[Int, Short]]", IsoTests(GenIso.fieldsTuple[TwoT[Int, Short]]))
 }
