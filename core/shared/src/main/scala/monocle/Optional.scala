6c21043cccc24cda6a91ab42835b65dfd033230a
Arman Bilge <armanbilge@gmail.com>
2022-10-13 16:47:07 +0000

    Fix core bincompat

--- core/shared/src/main/scala/monocle/Optional.scala
+++ core/shared/src/main/scala/monocle/Optional.scala
@@ -352,4 +352,8 @@ final case class OptionalSyntax[S, A](private val self: Optional[S, A]) extends 
 
   def at[I, A1](i: I)(implicit evAt: At[A, I, A1]): Optional[S, A1] =
     self.andThen(evAt.at(i))
+
+  @deprecated("Preserved for bincompat", "3.1.0")
+  def index[I, A1](i: I, evIndex: Index[A, I, A1]): Optional[S, A1] =
+    self.index(i)(evIndex, implicitly, implicitly)
 }
