6b99c235563350fadd1b04a9975e221205a330be
Ondra Pelech <ondra.pelech@gmail.com>
2022-11-27 17:53:40 +0100

    stringToURI

--- core/jvm/src/main/scala/monocle/std/PlatformSpecificString.scala
+++ core/jvm/src/main/scala/monocle/std/PlatformSpecificString.scala
@@ -1,10 +1,14 @@
 package monocle.std
 
-import java.net.URL
+import java.net.{URI, URL}
 import monocle.Prism
 import scala.util.Try
 
 private[std] trait PlatformSpecificStringOptics {
+  val stringToURI: Prism[String, URI] =
+    Prism((s: String) => Try(new URI(s)).toOption)(_.toString)
+  
+  /** Warning: `java.net.URL` is dangerous, prefer using `URI` with `stringToURI`. */
   val stringToURL: Prism[String, URL] =
     Prism((s: String) => Try(new URL(s)).toOption)(_.toString)
 }
